c4ef9c6a84836785da895db0bd7367bd1108645a
Spencer Kaiser <spencer.kaiser@aa.com>
2019-10-16 13:51:53 -0500

    Fixing tests for app, which failed because of a lack env variables

--- src/app.test.ts
+++ src/app.test.ts
@@ -3,6 +3,11 @@ import supertest from 'supertest';
 import app from './app';
 
 describe('GET /', () => {
+  beforeAll(() => {
+    process.env.SLACK_SIGNING_SECRET = 'A JUNK TOKEN';
+    process.env.SLACK_BOT_TOKEN = 'ANOTHER JUNK TOKEN';
+  });
+
   it('responds successfully', (done) => {
     supertest(app)
       .get('/')
