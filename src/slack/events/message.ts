e46102c93740ed33d4dc5c62f6f9296fae0b8633
Spencer Kaiser <spencer.kaiser@aa.com>
2019-11-01 17:35:13 -0500

    Updating dashboard to show team registration only when the toggle is set

--- src/slack/events/message.ts
+++ src/slack/events/message.ts
@@ -1,13 +1,23 @@
 import { App } from '@slack/bolt';
 import dashboardBlocks from '../blocks/dashboardBlocks';
+import { Config } from '../../entities/config';
+import logger from '../../logger';
 
 function register(bolt: App): void {
-  bolt.message(({ say }) => {
+  bolt.message(async ({ say }) => {
     // TODO: get context (toggles), use to generate specific dashboard blocks
-    say({
-      text: '',
-      blocks: dashboardBlocks,
-    });
+    const teamRegistrationToggle = (await Config.findOne({ key: 'teamRegistrationActive' })) || { key: 'teamRegistrationActive', value: 'false' };
+    const context = {
+      [teamRegistrationToggle.key]: teamRegistrationToggle.value === 'true',
+    };
+    try {
+      say({
+        text: '',
+        blocks: dashboardBlocks(context),
+      });
+    } catch (err) {
+      logger.error('Something went messaging the user...', err);
+    }
   });
 }
 
