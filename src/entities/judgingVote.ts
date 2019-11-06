676d17a40856cff5feb31e30c1fe36bf30a82861
Spencer Kaiser <spencer.kaiser@aa.com>
2019-11-06 16:36:43 -0600

    First pass at score calculation algorithm

--- src/entities/judgingVote.ts
+++ src/entities/judgingVote.ts
@@ -33,31 +33,106 @@ export class JudgingVote extends BaseEntity {
   currentTeamChosen: boolean;
 
   static async tabulate(): Promise<TeamResult[]> {
-    const votes = await JudgingVote.find();
-    const scores: { [id: string]: TeamScore } = {};
+    const allVotes = await JudgingVote.find();
+    const teams = await Team.find();
+    const numTeams = teams.length;
 
-    for (let i = 0; i < votes.length; i += 1) {
-      const vote = votes[i];
-      scores[vote.currentTeam] = updateTeamScoreWithVote(vote, scores[vote.currentTeam]);
+    // Initialize score keeping
+    const ascScores: { [id: string]: TeamScore } = {};
+    const descScores: { [id: string]: TeamScore } = {};
+    const calibrationCount: { [id: string]: number } = {};
+
+    // Use a designated percent of the votes for calibration
+    const avgNumVotesPerTeam = allVotes.length / numTeams;
+    const percentOfVotesToUseForCalibration = 0.1;
+    const percentBasedCalibrationVotes = Math.round(percentOfVotesToUseForCalibration * avgNumVotesPerTeam);
+    // Use percentOfVotesToUseForCalibration percent of the votes OR 2, whichever is bigger
+    const votesNeededForCalibration = Math.max(percentOfVotesToUseForCalibration, 2);
+
+    // If the number of votes needed to calibrate drops below 2 (e.g., less than 5 votes per team); throw error
+    // If the number of calibration votes is >= 50% of the votes for the team; throw error
+    if (percentBasedCalibrationVotes < 2 || votesNeededForCalibration / avgNumVotesPerTeam >= 0.5) {
+      throw new Error('Insufficient vote count for judging');
     }
 
-    const teamResults: TeamResult[] = await Promise.all(
-      Object.keys(scores).map(async (key) => {
-        const teamScore = scores[key];
-        const team = await Team.findOne({ id: teamScore.id });
-        return {
-          name: team.name,
-          ...teamScore,
-        };
-      }),
-    );
-    // TODO: Determine if ties are likely and figure out how to break them
+    for (let j = 0; j < 2; j += 1) {
+      // First pass is ascending, second pass is descending
+      const asc = j === 0;
+      // Reference corresponding scores dictionary
+      const scores = asc ? ascScores : descScores;
+      // Copy votes for iteration
+      const votes = Object.assign([], allVotes);
+
+      // Reverse to achieve descending order
+      if (!asc) {
+        votes.reverse();
+      }
+
+      let minScore = Number.POSITIVE_INFINITY;
+      let maxScore = Number.NEGATIVE_INFINITY;
+
+      for (let i = 0; i < votes.length; i += 1) {
+        const vote = votes[i];
+
+        const currentTeamId = vote.currentTeam;
+        const previousTeamId = vote.previousTeam;
+
+        const currentTeamScore = scores[currentTeamId] || { id: currentTeamId, score: 0 };
+        const previousTeamScore = scores[previousTeamId] || { id: previousTeamId, score: 0 };
+
+        // Update min and max score
+        minScore = Math.min(minScore, currentTeamScore.score, previousTeamScore.score);
+        maxScore = Math.max(maxScore, currentTeamScore.score, previousTeamScore.score);
+
+        if ((calibrationCount[currentTeamId] || 0) + (calibrationCount[previousTeamId] || 0) < votesNeededForCalibration * 2) {
+          // SIMPLE SCORING - one or both of the teams is uncalibrated
+          // Increment calibration count for each team
+          calibrationCount[currentTeamId] = calibrationCount[currentTeamId] ? calibrationCount[currentTeamId] + 1 : 1;
+          calibrationCount[previousTeamId] = calibrationCount[previousTeamId] ? calibrationCount[previousTeamId] + 1 : 1;
+
+          // Apply score impact based on vote outcome
+          const currentTeamScoreImpact = vote.currentTeamChosen ? 50 : -50;
+          currentTeamScore.score += currentTeamScoreImpact;
+          previousTeamScore.score += -currentTeamScoreImpact;
+        } else {
+          // ELO SCORING - both teams have been calibrated and have an ELO score
+          // In order to determine differential between scores, "shift" minScore to 0
+          const normalizationShift = minScore <= 0 ? Math.abs(minScore) : -minScore;
+          const normalizedMaxScore = maxScore + normalizationShift;
+
+          // Calculate team percentiles using the normalizationShift and normalizedMaxScore
+          const currentTeamPercentile = (currentTeamScore.score + normalizationShift) / normalizedMaxScore;
+          const previousTeamPercentile = (previousTeamScore.score + normalizationShift) / normalizedMaxScore;
+
+          // Determine whether the scoreImpact should be positive or negative
+          // Current Team Advantage: potential positive decreases, potential negative increases
+          const advantageCoeficient = currentTeamPercentile > previousTeamPercentile ? -1 : 1;
+          const currentTeamScoreImpact = advantageCoeficient * Math.round(100 * Math.abs(currentTeamPercentile - previousTeamPercentile));
+          const previousTeamScoreImpact = -advantageCoeficient * Math.round(100 * Math.abs(currentTeamPercentile - previousTeamPercentile));
+
+          // Apply score impact based on vote outcome
+          currentTeamScore.score += (vote.currentTeamChosen ? 100 : -100) + currentTeamScoreImpact;
+          previousTeamScore.score += (vote.currentTeamChosen ? -100 : 100) + previousTeamScoreImpact;
+        }
+
+        // Update scores for both teams
+        scores[currentTeamId] = currentTeamScore;
+        scores[previousTeamId] = previousTeamScore;
+      }
+    }
+
+    // Average scores from both passes
+    const teamResults: TeamResult[] = [];
+    Object.values(ascScores).forEach((teamScore) => {
+      const matchingTeam = teams.find((team) => team.id === teamScore.id);
+      teamResults.push({
+        id: matchingTeam.id,
+        name: matchingTeam.name,
+        score: Math.round((teamScore.score + descScores[teamScore.id].score) / 2),
+      });
+    });
+
+    // Sort and return final scores
     return teamResults.sort((a, b) => (a.score > b.score ? -1 : 1));
   }
-}
-
-function updateTeamScoreWithVote(vote: JudgingVote, teamScore?: TeamScore): TeamScore {
-  // TODO: Implement actual score calculation
-  const updatedTeamScore = teamScore || { id: vote.currentTeam, score: 0 };
-  return updatedTeamScore;
 }
