610dcf54526d299c7d6eb1af4ed8aa15324b5dc0
Spencer Kaiser <spencer.kaiser@aa.com>
2019-11-26 15:10:05 -0600

    Preventing the bot from responding to certain events, improving ack time for team registration

--- src/slack/events/message.ts
+++ src/slack/events/message.ts
@@ -4,17 +4,20 @@ import { Config } from '../../entities/config';
 import logger from '../../logger';
 
 function register(bolt: App): void {
-  bolt.message(async ({ say }) => {
+  bolt.message(async ({ say, message }) => {
     // TODO: get context (toggles), use to generate specific dashboard blocks
     const teamRegistrationToggle = (await Config.findOne({ key: 'teamRegistrationActive' })) || { key: 'teamRegistrationActive', value: 'false' };
     const context = {
       [teamRegistrationToggle.key]: teamRegistrationToggle.value === 'true',
     };
     try {
-      say({
-        text: '',
-        blocks: dashboardBlocks(context),
-      });
+      // Only respond if the message doesn't have a subtype (i.e., original user message event, deletion/edits are ignored)
+      if (!message.subtype) {
+        say({
+          text: '',
+          blocks: dashboardBlocks(context),
+        });
+      }
     } catch (err) {
       logger.error('Something went messaging the user...', err);
     }
