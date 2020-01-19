14f85ea73806cf5b870b85e4125b0730bed201c4
Spencer Kaiser <spencer.kaiser@aa.com>
2020-01-18 19:51:12 -0700

    Removing confetti because it's too fun. And because it breaks stuff

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,6 @@ class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" />
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         </Head>
         <body>
           <Main />
