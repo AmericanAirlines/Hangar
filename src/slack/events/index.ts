7df09e4ec982cea189b600f9c65c134b9cc3d8c2
Dhruv Tailor <dhruv61998@gmail.com>
2020-09-22 17:34:42 -0500

    recommended changes

--- src/slack/events/index.ts
+++ src/slack/events/index.ts
@@ -1,8 +1,10 @@
 import { App } from '@slack/bolt';
 import logger from '../../logger';
 import message from './message';
+import event from './event';
 
 export default function register(app: App): void {
   logger.info('Registering event listeners');
   message(app);
+  event(app);
 }
