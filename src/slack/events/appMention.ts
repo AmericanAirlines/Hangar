009da2bdea6664b4a367168a0fac2cdc2f83eec9
Dhruv Tailor <dhruv61998@gmail.com>
2020-09-23 20:44:27 -0500

    Adds functionality to respond with helpful information if tagged in a public channel (#157)
    * tagged functionality
    
    * Apply suggestions from code review
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
    
    * recommended changes
    
    * updated to work with the new file structures
    
    * Apply suggestions from code review
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
    
    * updated to reply in thread
    
    * Apply suggestions from code review
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
    
    * prettier?
    
    Co-Authored-By: Oscar Goddard <52873114+oscargoddard@users.noreply.github.com>
    
    * Revert "prettier?"
    
    This reverts commit 2c2d9e069455205baae03d0decf471875079d720.
    
    * possible fix
    
    Co-Authored-By: Oscar Goddard <52873114+oscargoddard@users.noreply.github.com>
    
    * Update appMention.ts
    
    Co-Authored-By: Oscar Goddard <52873114+oscargoddard@users.noreply.github.com>
    
    * Update appMention.ts
    
    Co-Authored-By: Oscar Goddard <52873114+oscargoddard@users.noreply.github.com>
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
    Co-authored-by: Oscar Goddard <52873114+oscargoddard@users.noreply.github.com>

---
+++ src/slack/events/appMention.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { Middleware, SlackEventMiddlewareArgs } from '@slack/bolt';
+import { app } from '..';
+import logger from '../../logger';
+
+export const appMention: Middleware<SlackEventMiddlewareArgs<'app_mention'>> = async ({ context, payload }) => {
+  try {
+    await app.client.chat.postMessage({
+      token: context.botToken,
+      channel: payload.channel,
+      thread_ts: payload.ts,
+      text:
+        "Hey there :wave: I can help your team during the hackathon! To see all of the things I can help with, simply click/tap my name and chose 'Go to App' :tada:`",
+    });
+  } catch (error) {
+    logger.error('Something went wrong responding to a bot mention: ', error);
+  }
+};
