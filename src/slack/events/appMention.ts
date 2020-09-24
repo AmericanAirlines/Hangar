03af1bcb6c2f8e901243d473e3081558cb8df77d
Dhruv Tailor <dhruv61998@gmail.com>
2020-09-23 19:55:30 -0500

    possible fix
    Co-Authored-By: Oscar Goddard <52873114+oscargoddard@users.noreply.github.com>

--- src/slack/events/appMention.ts
+++ src/slack/events/appMention.ts
@@ -3,17 +3,16 @@ import { Middleware, SlackEventMiddlewareArgs } from '@slack/bolt';
 import { app } from '..';
 import logger from '../../logger';
 
-export const appMention: Middleware<SlackEventMiddlewareArgs<'app_mention'>> = async ({ event, context, payload}) =>{
-
+export const appMention: Middleware<SlackEventMiddlewareArgs<'app_mention'>> = async ({ event, context, payload }) => {
   try {
-  await app.client.chat.postMessage({
-    token: context.botToken,
-    channel: payload.channel,
-    thread_ts: payload.ts,
-    text: "Hey there :wave: I can help your team during the hackathon! To see all of the things I can help with, simply click/tap my name and chose 'Go to App' :tada:`",
-  });
-  
+    await app.client.chat.postMessage({
+      token: context.botToken,
+      channel: payload.channel,
+      thread_ts: payload.ts,
+      text:
+        "Hey there :wave: I can help your team during the hackathon! To see all of the things I can help with, simply click/tap my name and chose 'Go to App' :tada:`",
+    });
   } catch (error) {
     logger.error('Something went wrong responding to a bot mention: ', error);
   }
-}
+};
