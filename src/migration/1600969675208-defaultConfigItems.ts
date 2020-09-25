bdd62874349714ae111304533559dd6efabf8994
Smit Shah <shah.smit@hotmail.com>
2020-09-25 10:50:25 -0500

    installed @type/node and changed the value to key to false

--- src/migration/1600969675208-defaultConfigItems.ts
+++ src/migration/1600969675208-defaultConfigItems.ts
@@ -1,23 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class defaultConfigItems1600969675208 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query('INSERT INTO "configuration" ("key", "value") VALUES ("teamRegistrationActive", "false")', undefined);
+    await queryRunner.query('INSERT INTO "configuration" ("key", "value") VALUES ("supportRequestQueueActive", "false")', undefined);
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(
-            'INSERT INTO "configuration" ("key", "value") VALUES ("teamRegistrationActive", "true")', undefined
-          );
-        await queryRunner.query(
-            'INSERT INTO "configuration" ("key", "value") VALUES ("supportRequestQueueActive", "true")', undefined
-        );
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(
-            'DELETE FROM "configuration" WHERE key="teamRegistrationActive" AND value="true"', undefined
-          );
-        await queryRunner.query(
-            'DELETE FROM "configuration" WHERE key="supportRequestQueueActive" AND value="ture"', undefined
-        );
-        
-    }
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query('DELETE FROM "configuration" WHERE key="teamRegistrationActive" AND value="false"', undefined);
+    await queryRunner.query('DELETE FROM "configuration" WHERE key="supportRequestQueueActive" AND value="false"', undefined);
+  }
 }
