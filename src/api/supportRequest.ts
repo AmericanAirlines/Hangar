f18f21c127553af443ebd38380ba2b7705de5aa3
Maha Siddique <mahasiddique@WayneEnterprise.lan1>
2020-10-07 11:27:01 -0500

    This adds get specific functionality

--- src/api/supportRequest.ts
+++ src/api/supportRequest.ts
@@ -11,6 +11,10 @@ export interface NextSupportRequestResponse {
   supportRequest: SupportRequest;
   userNotified: boolean;
 }
+supportRequestRoutes.get('/getAll', async (req, res) => {
+  const requests = await SupportRequest.find();
+  res.send(requests);
+});
 
 supportRequestRoutes.get('/getCount', async (req, res) => {
   const ideaCount = await SupportRequest.count({ type: SupportRequestType.IdeaPitch, status: SupportRequestStatus.Pending });
@@ -125,4 +129,61 @@ supportRequestRoutes.post('/abandonRequest', async (req, res) => {
     res.status(500).send('Unable to close request');
     logger.error(err);
   }
+});
+supportRequestRoutes.patch('/getSpecific', async (req, res) => {
+  const { supportRequestId, adminName } = req.body;
+
+  if(!supportRequestId) {
+    res.status(400).send("Property 'supportRequestId' is required");
+    return;
+  } else if (!adminName || !adminName.trim()) {
+    res.status(400).send("Property 'adminName' is required");
+    return;
+
+  }
+  const request = await SupportRequest.findOne(supportRequestId);
+
+  if(!request || request.status != SupportRequestStatus.Pending) {
+    res.status(400).send("The support request entered is not valid")
+    return;
+  }
+
+  try {
+    await SupportRequest.createQueryBuilder('supportRequest')
+    .update()
+    .set({
+      status: SupportRequestStatus.InProgress,
+    })
+    .where({
+      id: supportRequestId,
+      status: SupportRequestStatus.Pending,
+    })
+    .execute();
+
+    res.sendStatus(200);
+ } catch (error) {
+    res.status(500).send('Unable To Open A Specific Request');
+ }
+
+ let userNotified = false;
+
+ try {
+   if (request) {
+     await messageUsers(
+       [request.slackId],
+       `:tada: ${adminName} is ready to ${
+         request.type === SupportRequestType.IdeaPitch ? 'help you with an idea' : 'help with your technical issue'
+       }, so head over to our booth. Feel free to bring other members of your team and make sure to bring your laptop if relevant.\n\nWhen you arrive, tell one of our team members that you're here to meet with *${adminName}*!`,
+     );
+     userNotified = true;
+   }
+ } catch (err) {
+   logger.error("Unable to notify users they're support request has been served", err);
+ }
+
+ const response: NextSupportRequestResponse = {
+  userNotified,
+  supportRequest: request,
+};
+  res.send(response);
 });
