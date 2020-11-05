1759d16358ec5783220c2a360588bf07e9fec2e5
John Kahn <hello@jkahn.me>
2020-11-04 23:23:53 -0600

    Create Discord client for bot

---
+++ src/discord/index.ts
@@ -0,0 +1,26 @@
+import Discord from 'discord.js';
+
+export const client = new Discord.Client();
+
+client.on('message', (msg) => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+export const setupDiscord = async (token: string): Promise<Discord.Client> => {
+  const readyPromise = new Promise((resolve) => {
+    const readyListener = (): void => {
+      resolve();
+      client.off('error', readyListener);
+    };
+
+    client.on('ready', readyListener);
+  });
+
+  await client.login(token);
+
+  await readyPromise;
+
+  return client;
+};
