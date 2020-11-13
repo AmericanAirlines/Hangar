ab4977b99a610c5982379c998d70b70cff86d26d
Austin Slobodnik <austinslobodnik@gmail.com>
2020-11-13 12:40:24 -0600

    fixes help issues

--- src/tests/discord/events/message/help.test.ts
+++ src/tests/discord/events/message/help.test.ts
@@ -1,22 +1,40 @@
 import Discord from 'discord.js';
 import 'jest';
+import { commands } from '../../../../discord/events/message';
 import { help } from '../../../../discord/events/message/help';
 
-describe('help handler', () => {
-  it('will DM the user a list of commands and respond with a notification msg if the msg came form a channel', async () => {
-    const reply = jest.fn();
-    const send = jest.fn();
-    const getHelp = ({
-      reply,
-      content: '!help',
-      author: {
-        send,
-        id: 'JaneSmith',
-      },
-    } as unknown) as Discord.Message;
+const getHelpMsg = () =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  (({
+    reply: jest.fn(),
+    content: '!help',
+    author: {
+      send: jest.fn(),
+      id: 'JaneSmith',
+    },
+    channel: { type: 'dm' },
+  } as unknown) as Discord.Message);
 
-    await help(getHelp);
-    expect(reply).toBeCalledTimes(1);
-    expect(send).toBeCalledTimes(1);
+describe('help handler', () => {
+  it('will DM the user a list of commands', async () => {
+    const msg = getHelpMsg();
+    await help(msg);
+    expect(msg.reply).not.toHaveBeenCalled();
+    expect(msg.author.send).toBeCalledTimes(1);
+  });
+
+  it('will DM the user a list of commands and respond with a notification msg if the msg came from a channel', async () => {
+    const msg = getHelpMsg();
+    msg.channel.type = 'news';
+    await help(msg);
+    expect(msg.reply).toBeCalledTimes(1);
+    expect(msg.author.send).toBeCalledTimes(1);
+  });
+
+  it('responds with the right number of commands', async () => {
+    const msg = getHelpMsg();
+    await help(msg);
+    const args = (msg.author.send as jest.Mock).mock.calls[0][0];
+    expect(args.embed.fields).toHaveLength(commands.length);
   });
 });
