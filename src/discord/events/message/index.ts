3d2bbc77779345152f012a2ab3c955ad65f14184
Austin Slobodnik <austinslobodnik@gmail.com>
2020-11-30 13:25:02 -0600

    Implements the registerTeam command in discord

--- src/discord/events/message/index.ts
+++ src/discord/events/message/index.ts
@@ -4,6 +4,7 @@ import { ping } from './ping';
 import { client } from '../..';
 import logger from '../../../logger';
 import { help } from './help';
+import { registerTeam, getUsers } from './registerTeam';
 
 interface Command {
   handlerId: string;
@@ -25,39 +26,47 @@ export const commands: Command[] = [
     description: 'Lists commands the user can use to interact with the bot',
     handler: help,
   },
+  {
+    handlerId: 'registerTeam',
+    trigger: '!registerTeam',
+    description: 'Leads the user through the process of registering a team',
+    handler: registerTeam,
+  },
 ];
 
 export async function message(msg: Discord.Message): Promise<void> {
-  // Make sure the bot doesn't respond to itself
-  if (msg.author.id === client.user.id) return;
+  if (!getUsers().includes(msg.author.id)) {
+    // Make sure the bot doesn't respond to itself
+    if (msg.author.id === client.user.id) return;
 
-  // If not in a DM, check to make sure it's in one of the approved channels
-  const botChannelIds = (process.env.DISCORD_BOT_CHANNEL_IDS ?? '').split(',').map((id) => id.trim());
-  if (msg.channel.type !== 'dm' && !botChannelIds.includes(msg.channel.id)) return;
+    // If not in a DM, check to make sure it's in one of the approved channels
+    const botChannelIds = (process.env.DISCORD_BOT_CHANNEL_IDS ?? '').split(',').map((id) => id.trim());
+    if (msg.channel.type !== 'dm' && !botChannelIds.includes(msg.channel.id)) return;
 
-  let context = await DiscordContext.findOne(msg.author.id);
+    let context = await DiscordContext.findOne(msg.author.id);
 
-  if (!context) {
-    context = new DiscordContext(msg.author.id, '');
+    if (!context) {
+      context = new DiscordContext(msg.author.id, '');
+    }
+
+    let command: Command | undefined;
+
+    // Check to see if the context has a next step
+    if (context.nextStep) {
+      command = commands.find((c) => c.handlerId === context.nextStep);
+      logger.error('Discord context next step handler not found');
+    }
+
+    // If not, try to match the content to a known trigger id
+    if (!command) {
+      command = commands.find((c) => c.trigger === msg.content.trim());
+    }
+
+    if (command) {
+      await command.handler(msg, context);
+      return;
+    }
+
+    msg.reply("I'm not sure what you need, try replying with `!help` for some useful information!");
   }
-
-  let command: Command | undefined;
-
-  // Check to see if the context has a next step
-  if (context.nextStep) {
-    command = commands.find((c) => c.handlerId === context.nextStep);
-    logger.error('Discord context next step handler not found');
-  }
-
-  // If not, try to match the content to a known trigger id
-  if (!command) {
-    command = commands.find((c) => c.trigger === msg.content.trim());
-  }
-
-  if (command) {
-    await command.handler(msg, context);
-    return;
-  }
-
-  msg.reply("I'm not sure what you need, try replying with `!help` for some useful information!");
 }
