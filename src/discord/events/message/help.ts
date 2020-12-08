a01dc57c5c617638666fe221daf6ffa33ae4e38d
John Kahn <7807353+johnkahn@users.noreply.github.com>
2020-12-08 11:39:41 -0600

    Add hidden help items (#235)

--- src/discord/events/message/help.ts
+++ src/discord/events/message/help.ts
@@ -2,16 +2,20 @@ import Discord from 'discord.js';
 import { commands } from '.';
 import { colors } from '../../constants';
 
+export const hiddenHandlers = ['ping'];
+
 export async function help(msg: Discord.Message): Promise<void> {
   await msg.author.send({
     embed: {
       color: colors.info,
       title: '**Welcome to the Hangar bot** :wave:',
       description: `Listed below are various commands that you can use to interact with the bot!\n\nIf you are looking for more information in regard to this challenge, [Click Here](${process.env.CHALLENGE_URL})`,
-      fields: commands.map((command) => ({
-        name: `**\n**\`${command.trigger}\``,
-        value: command.description,
-      })),
+      fields: commands
+        .filter((command) => !hiddenHandlers.includes(command.handlerId))
+        .map((command) => ({
+          name: `**\n**\`${command.trigger}\``,
+          value: command.description,
+        })),
     },
   });
   if (msg.channel.type !== 'dm') {
