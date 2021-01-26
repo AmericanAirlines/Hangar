8caaddb071a595e2037e66a1470eaa0091692af2
Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
2021-01-26 13:02:31 -0600

    Display teams endpoint (#250)
    * registerTeamRefactor
    
    * TeamRegisterRefactor
    
    * AddsProperMigrations
    
    * getAllTeamsEndpoint
    
    * Adding front end for team display
    
    * Update src/api/team.ts
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
    
    * Update src/components/RegisteredTeamsList.tsx
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
    
    * Not needed, just used to test the endpoint
    
    * Currently not used to commented out
    
    Unsure if we want this implemented later on so I did not delete it
    
    * Will be needed eventually so commented out
    
    * Didn't comment out properly :^)
    
    Co-authored-by: Austin Slobodnik <austinslobodnik@gmail.com>

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { requireAuth } from './middleware/requireAuth';
+import { team } from './team';
 import { supportRequestRoutes } from './supportRequest';
 import { Judge } from '../entities/judge';
 import { Team } from '../entities/team';
@@ -79,6 +80,7 @@ api.post('/skip', async (req, res) => {
 });
 
 api.use('/supportRequest', requireAuth(), supportRequestRoutes);
+api.use('/team', requireAuth(), team);
 api.use('/judging', requireAuth(), judging);
 api.use('/config', requireAuth(), config);
 api.use('/admin', admin);
---
+++ src/api/team.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { Team } from '../entities/team';
+import logger from '../logger';
+
+export const team = express.Router();
+
+team.get('/getAll', async (req, res) => {
+  try {
+    const teamList = await Team.find();
+    res.status(200).send(teamList);
+  } catch (error) {
+    logger.error('Unable to retrieve team list: ', error);
+    res.status(500).send('Unable to retrieve teams from database');
+  }
+});
