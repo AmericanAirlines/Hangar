f82d9e576e362919ecc7b06cdf71c896c3e088e5
Austin Slobodnik <austinslobodnik@gmail.com>
2021-02-08 15:37:24 -0600

    simpleEnvSetup

---
+++ src/env.ts
@@ -0,0 +1,23 @@
+// src/env.ts
+import setEnv from '@americanairlines/simple-env';
+
+const env = setEnv({
+  required: {
+    nodeEnv: 'NODE_ENV',
+    judgeSecret: 'JUDGE_SECRET',
+    supportSecret: 'SUPPORT_SECRET',
+    adminSecret: 'ADMIN_SECRET',
+    challengeUrl: 'CHALLENGE_URL',
+  },
+  optional: {
+    dbSSLDisabled: 'DB_SSL_DISABLED',
+    slackBotToken: 'SLACK_BOT_TOKEN',
+    slackSigningSecret: 'SLACK_SIGNING_SECRET',
+    discordBotToken: 'DISCORD_BOT_TOKEN',
+    discordChannelIds: 'DISCORD_BOT_CHANNEL_IDS',
+    pgUser: 'PGUSER',
+    pgPassword: 'PGPASSWORD',
+  },
+});
+
+export default env;
