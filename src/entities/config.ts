f5dd00b4555c2ab848516fce86832e2a8f3826ba
Tristen Even <tristengeven@gmail.com>
2021-03-15 23:58:18 -0500

    make column type simple-json. create getValueAs methods.

--- src/entities/config.ts
+++ src/entities/config.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-dupe-class-members */
+
 import { Entity, PrimaryColumn, Column, BaseEntity } from 'typeorm';
 
 @Entity('configuration')
@@ -12,17 +14,84 @@ export class Config extends BaseEntity {
   @PrimaryColumn()
   key: string;
 
-  @Column()
-  value: string;
+  @Column({ type: 'simple-json', nullable: true, default: null })
+  value: string | number | boolean | null;
 
   static async findToggleForKey(key: string): Promise<boolean> {
     const toggle = await this.findOne({ key });
-    if (toggle) {
+    // QUESTION: I believe I should still check if toggle is null here?
+    if (toggle && typeof toggle.value === 'string') {
       toggle.value = toggle.value.toLowerCase();
       if (toggle.value !== 'false' && toggle.value !== 'true') {
         throw new Error('Config item found but cannot be cast to boolean');
       }
     }
     return toggle ? toggle.value === 'true' : false;
+  }
+
+  static async getValueAs(key: string, valueType: 'string', shouldThrow: true): Promise<string>;
+
+  static async getValueAs(key: string, valueType: 'boolean', shouldThrow: true): Promise<boolean>;
+
+  static async getValueAs(key: string, valueType: 'number', shouldThrow: true): Promise<number>;
+
+  static async getValueAs(key: string, valueType: 'string', shouldThrow: false): Promise<string> | null;
+
+  static async getValueAs(key: string, valueType: 'boolean', shouldThrow: false): Promise<boolean> | null;
+
+  static async getValueAs(key: string, valueType: 'number', shouldThrow: false): Promise<number> | null;
+
+  static async getValueAs(key: string, valueType: 'string' | 'boolean' | 'number', shouldThrow: boolean): Promise<string | boolean | number> | null {
+    const something = await this.findOne({ key });
+
+    if (something.value === null) {
+      if (shouldThrow) {
+        throw new Error('Value was null');
+      } else {
+        return null;
+      }
+    }
+
+    switch (valueType) {
+      case 'string': {
+        const castValueAsString: string = something.value as string;
+        if (castValueAsString === null) {
+          if (shouldThrow) {
+            throw new Error('Value could not be cast to string');
+          } else {
+            return null;
+          }
+        } else {
+          return castValueAsString;
+        }
+      }
+      case 'boolean': {
+        const castValueAsBool: boolean = something.value as boolean;
+        if (castValueAsBool === null) {
+          if (shouldThrow) {
+            throw new Error('Value could not be cast to boolean');
+          } else {
+            return null;
+          }
+        } else {
+          return castValueAsBool;
+        }
+      }
+      case 'number': {
+        const castValueAsNumber: number = something.value as number;
+        if (castValueAsNumber === null) {
+          if (shouldThrow) {
+            throw new Error('Value could not be cast to boolean');
+          } else {
+            return null;
+          }
+        } else {
+          return castValueAsNumber;
+        }
+      }
+      default: {
+        throw new Error('no appropriate valueType detected');
+      }
+    }
   }
 }
