0283ecf5892f78a3d1f7d2269705d448a8320aeb
Demondre Livingston <livingstondemondre7@gmail.com>
2021-03-31 15:50:10 -0400

    Fixing: Eslint is not checking tsx files -  #217 (#335)
    * Fixing issues #:
    Eslint is not checking tsx files #217
    Added "tsx" & "jsx" to the eslint accepted extensions
    Added "jsx: preserved" to allow tsx translation to jsx without error
    Fixed typing error in nodemonConfig delay
    
    * Fixing issues #:
    Eslint is not checking tsx files #217
    Added "tsx" & "jsx" to the eslint accepted extensions
    Added "jsx: preserved" to allow tsx translation to jsx without error
    Fixed typing error in nodemonConfig delay
    Fixed syntax to be Github friendly
    Added eslintIgnore to remove the build files from linting
    
    * Fixing issues #:
    Eslint is not checking tsx files #217
    Added "tsx" & "jsx" to the eslint accepted extensions
    Added "jsx: preserved" to allow tsx translation to jsx without error
    Fixed typing error in nodemonConfig delay
    Fixed syntax to be Github friendly
    Added eslintIgnore to remove the build files from linting
    Adding my name to the Authors.md file
    Migrating all eslint configs to the package.json file for consolidation
    
    * Fixing issues #:
    Eslint is not checking tsx files #217
    Added "tsx" & "jsx" to the eslint accepted extensions
    Added "jsx: preserved" to allow tsx translation to jsx without error
    Fixed typing error in nodemonConfig delay
    Fixed syntax to be Github friendly
    Added eslintIgnore to remove the build files from linting
    Adding my name to the Authors.md file
    
    * Fixing issues #:
    Eslint is not checking tsx files #217
    Added "tsx" & "jsx" to the eslint accepted extensions
    Added "jsx: preserved" to allow tsx translation to jsx without error
    Fixed typing error in nodemonConfig delay
    Fixed syntax to be Github friendly
    Added eslintIgnore to remove the build files from linting
    Adding my name to the Authors.md file
    Adding back .eslintrc.js
    
    * Fixing issues #:
    Eslint is not checking tsx files #217
    Added "tsx" & "jsx" to the eslint accepted extensions
    Added "jsx: preserved" to allow tsx translation to jsx without error
    Fixed typing error in nodemonConfig delay
    Fixed syntax to be Github friendly
    Added eslintIgnore to remove the build files from linting
    Adding my name to the Authors.md file
    Adding back .eslintrc.js
    Removing .eslintignore file
    Switching linting to only capture files within src directory
    Removing unneeded tsconfig
    
    * Apply suggestions from code review
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
    
    * Update tsconfig.json
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.js', '.ts'],
+        extensions: ['.js', '.ts', '.tsx', '.jsx'],
       },
     },
     react: {
