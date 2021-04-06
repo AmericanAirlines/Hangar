2b24a4e58678087978ffa9b7206b51fa711c698f
Demondre Livingston <livingstondemondre7@gmail.com>
2021-04-06 12:26:47 -0400

    Merge branch 'Config-Refactor' of /Users/demondrelivingston/Documents/GitHub/Hangar with conflicts.

---
+++ src/types/config.ts
@@ -0,0 +1,7 @@
+export enum ConfigList {
+  AdminSecret = 'adminSecret',
+  JobChatQueueActive = 'jobChatQueueActive',
+  SupportRequestQueueActive = 'supportRequestQueueActive',
+  SupportSecret = 'supportSecret',
+  TeamRegistrationActive = 'teamRegistrationActive',
+}
