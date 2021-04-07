c23044ace00c91c9f910ef0e0d6c3cdfcda618b4
Demondre Livingston <livingstondemondre7@gmail.com>
2021-04-07 16:08:10 -0400

    Adding an open source label to a footer in the main admin page

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,2 @@
-declare module '*.scss' {
-  const classes: { [key: string]: string};
-  export default classes;
-}
-/// <reference types="next" />
-/// <reference types="next/types/global" />
+// / <reference types="next" />
+// / <reference types="next/types/global" />
