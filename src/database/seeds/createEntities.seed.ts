e8c5e973882400cd1e166aa7807446646bf8b11a
Olin <olin.zhou.0@gmail.com>
2021-04-06 20:52:13 -0500

    added schema:reset to drop and sync the db, changed team description to their expected placements in judging

--- src/database/seeds/createEntities.seed.ts
+++ src/database/seeds/createEntities.seed.ts
@@ -1,11 +1,25 @@
 /* eslint-disable class-methods-use-this */
+/* eslint-disable no-param-reassign */
+
 import { Seeder, Factory } from 'typeorm-seeding';
 import { Judge } from '../../entities/judge';
 import { Team } from '../../entities/team';
 
+const numJudges = 10;
+const numTeams = 10;
+// it makes more sense to use a set here, but getting random element from set takes O(N) in javascript :(
+const placements: number[] = Array.from(Array(numTeams).keys()).map((n) => n + 1);
+
 export default class CreateEntities implements Seeder {
   public async run(factory: Factory): Promise<void> {
-    await factory(Judge)().createMany(10);
-    await factory(Team)().createMany(10);
+    await factory(Judge)().createMany(numJudges);
+    await factory(Team)()
+      .map(async (team: Team) => {
+        const i = Math.floor(Math.random() * placements.length);
+        team.projectDescription = String(placements[i]);
+        placements.splice(i, 1);
+        return team;
+      })
+      .createMany(numTeams);
   }
 }
