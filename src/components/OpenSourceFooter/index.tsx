6c2c2f375302b3889b5d476f45ad6bf80353e730
Demondre Livingston <livingstondemondre7@gmail.com>
2021-04-07 16:08:10 -0400

    Adding an open source label to a footer in the main admin page

--- src/components/OpenSourceFooter/index.tsx
+++ src/components/OpenSourceFooter/index.tsx
@@ -1,25 +1,21 @@
-import { Grid } from '@material-ui/core';
 import React from 'react';
 import styles from './open-source-footer.module.scss';
 
 const OpenSourceFooter = (): JSX.Element => (
   <div className={styles.footer}>
-    <Grid container spacing={5} alignItems='center' justify='center'>
-      <Grid item>
-        <span className={styles['footer-text']}>Hangar is an open source project created my American Airlines.<br />
-          <a className={styles['footer-text']} href='https://github.com/AmericanAirlines/Hangar/issues/new/choose'
-             target='_blank' rel="noreferrer"> Suggest a feature or report a bug</a>
-        </span>
-      </Grid>
-      <Grid item>
-        <p>
-          <a href='https://github.com/AmericanAirlines/Hangar' target='_blank' rel="noreferrer">
-            <img alt='GitHub Repo stars'
-                 src='https://img.shields.io/github/stars/AmericanAirlines/Hangar?label=Github&style=social' />
-          </a>
-        </p>
-      </Grid>
-    </Grid>
+    <div className='container d-flex justify-content-between align-items-center'>
+      <div>
+        Hangar is an open source project created by American Airlines.<br />
+        <a href='https://github.com/AmericanAirlines/Hangar/issues/new/choose'
+           target='_blank' rel='noreferrer'>Suggest a feature or report a bug</a>
+      </div>
+      <div>
+        <a href='https://github.com/AmericanAirlines/Hangar' target='_blank' rel='noreferrer'>
+          <img alt='GitHub Repo stars' width="125"
+               src='https://img.shields.io/github/stars/AmericanAirlines/Hangar.svg?label=Github&style=social' />
+        </a>
+      </div>
+    </div>
   </div>
 );
 
