f442df31d11314c9ac129383f7bdbd90f6d5e392
Demondre Livingston <livingstondemondre7@gmail.com>
2021-04-07 16:08:10 -0400

    Adding an open source label to a footer in the main admin page

--- src/components/OpenSourceFooter/index.tsx
+++ src/components/OpenSourceFooter/index.tsx
@@ -8,12 +8,12 @@ const OpenSourceFooter = (): JSX.Element => (
       <Grid item>
         <span className={styles['footer-text']}>Hangar is an open source project created my American Airlines.<br />
           <a className={styles['footer-text']} href='https://github.com/AmericanAirlines/Hangar/issues/new/choose'
-             target='_blank'> Suggest a feature or report a bug</a>
+             target='_blank' rel="noreferrer"> Suggest a feature or report a bug</a>
         </span>
       </Grid>
       <Grid item>
         <p>
-          <a href='https://github.com/AmericanAirlines/Hangar' target='_blank'>
+          <a href='https://github.com/AmericanAirlines/Hangar' target='_blank' rel="noreferrer">
             <img alt='GitHub Repo stars'
                  src='https://img.shields.io/github/stars/AmericanAirlines/Hangar?label=Github&style=social' />
           </a>
