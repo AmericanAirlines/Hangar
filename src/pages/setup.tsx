02bf19aade386956ca6e49e0e30a56bce03c264f
Tatum Hutton <tatumhutton@gmail.com>
2021-04-22 15:56:27 -0500

    Added yup validation and input descriptions

--- src/pages/setup.tsx
+++ src/pages/setup.tsx
@@ -1,9 +1,20 @@
 /* global window, fetch */
 import React from 'react';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import { config } from '../api/config';
 
 const SetupPage: React.FC = () => {
+  const SignupSchema = Yup.object().shape({
+    adminSecret: Yup.string()
+      .min(6, 'Too Short!')
+      .max(20, 'Too Long!')
+      .required('Required'),
+    discordChannelIds: Yup.string(),
+    discordBotToken: Yup.string(),
+    slackBotToken: Yup.string(),
+    slackSigningSecret: Yup.string(),
+  });
   const formik = useFormik({
     initialValues: {
       secret: '',
@@ -13,6 +24,7 @@ const SetupPage: React.FC = () => {
       slackBotToken: '',
       slackSigningSecret: '',
     },
+    validationSchema: SignupSchema,
     async onSubmit(values) {
       // TODO: Update all values in the database, and redirect to admin page
       // QUESTION: Should I bundle the form data into multiple POST requests to the config api, or should I connect this entity directly to the database similarly to the ConfigItem?
@@ -23,23 +35,26 @@ const SetupPage: React.FC = () => {
   return (
     <div className="container mt-4">
       <div className="row">
-        <div className="col-md-4 offset-md-4">
+        <div className="col-md-6 offset-md-3">
           <h1 className="display-2 text-center">Setup</h1>
-          <h4 className="display-6 text-center">Enter your respective values for the configuration items below, then press submit.</h4>
+          <h4 className="display-6 text-center font-weight-light">Enter your respective values for the configuration items below, then press submit. </h4>
+          <h4 className="display-6 text-center">If using Slack, leave Discord fields blank. If using Discord, leave Slack fields blank. </h4>
           <form onSubmit={formik.handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="secret">adminSecret</label>
+            <div className="form-group mt-3">
+              <label htmlFor="secret">Enter the Admin Secret. Must be between 6-20 characters</label>
               <input
                 id="adminSecret"
                 type="password"
-                placeholder="Shhh..."
+                placeholder="enter value..."
                 className={`form-control ${formik.errors.adminSecret ? 'is-invalid' : ''}`}
                 value={formik.values.adminSecret}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
               />
+            </div>
               {/* QUESTION: what is the 'htmlFor' below? Should I change these to match their respective content? */}
-              <label htmlFor="discordChannelIds">discordChannelIds</label>
+            <div className="form-group">
+              <label htmlFor="discordChannelIds">Discord: Enter Discord Channel ID. If multiple IDs, separate with a comma.</label>
               <input
                 id="discordChannelIds"
                 placeholder="id1, id2, id3"
@@ -48,7 +63,9 @@ const SetupPage: React.FC = () => {
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
               />
-              <label htmlFor="secret">discordBotToken</label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="secret">Discord: Enter the Discord Bot Token</label>
               <input
                 id="discordBotToken"
                 placeholder="enter value..."
@@ -57,7 +74,9 @@ const SetupPage: React.FC = () => {
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
               />
-              <label htmlFor="secret">slackBotToken</label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="secret">Slack: Enter the Slack Bot Token</label>
               <input
                 id="slackBotToken"
                 placeholder="enter value..."
@@ -66,7 +85,9 @@ const SetupPage: React.FC = () => {
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
               />
-              <label htmlFor="secret">slackSigningSecret</label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="secret">Slack: Enter the Slack Signing Secret</label>
               <input
                 id="slackSigningSecret"
                 placeholder="enter value..."
@@ -79,7 +100,7 @@ const SetupPage: React.FC = () => {
               {/* QUESTION: What is this below? */}
               {/* <div className="invalid-feedback">{formik.errors.secret}</div> */}
             </div>
-            <button type="submit" className="btn btn-dark float-right">
+            <button type="submit" className="btn btn-primary btn-block mb-5">
               Submit
             </button>
           </form>
