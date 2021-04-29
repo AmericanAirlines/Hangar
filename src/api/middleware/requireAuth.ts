9a44e429b27503b2dcbae335f2aad7c81352af56
Razvan Preotu <rnp170130@utdallas.edu>
2021-04-28 21:04:59 -0400

    updated requreAuth.ts to include setup logic

--- src/api/middleware/requireAuth.ts
+++ src/api/middleware/requireAuth.ts
@@ -1,22 +1,40 @@
 import { Handler } from 'express';
 import logger from '../../logger';
 import { Config } from '../../entities/config';
+import { getActivePlatform } from '../../common/index';
+
+import url = require('url');
 
 export const requireAuth = (redirect = false): Handler => async (req, res, next): Promise<void> => {
   const adminSecret = await Config.getValueAs('adminSecret', 'string', false);
   const supportSecret = await Config.getValueAs('supportSecret', 'string', false);
-  if (
+
+  const activePlatform = await getActivePlatform();
+  const appSetupComplete = adminSecret && activePlatform !== null;
+  const path = url.parse(req.url).pathname;
+
+  if (path === '/setup') {
+    if (appSetupComplete) {
+      res.redirect('/');
+    } else {
+      next();
+    }
+  } else if (path === '/bulk') {
+    if (!appSetupComplete) {
+      next();
+    } else {
+      res.end();
+    }
+  } else if (!appSetupComplete) {
+    res.redirect('/setup');
+  } else if (
     req.signedCookies?.authed === 'yes' ||
     (adminSecret && req.headers.authorization === adminSecret) ||
     (supportSecret && req.headers.authorization === supportSecret)
   ) {
     next();
   } else if (redirect) {
-    if (!adminSecret) {
-      res.redirect('/setup');
-    } else {
-      res.redirect('/login');
-    }
+    res.redirect('/login');
   } else {
     res.sendStatus(401);
     logger.error(
