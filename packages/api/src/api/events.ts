0bd699811fed573618085ba8aadcfff81d15081e
Demondre Livingston <livingstondemondre7@gmail.com>
2021-10-21 13:40:11 -0400

    initial commit for adding the event api endpoint

---
+++ packages/api/src/api/events.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { QueryOrder } from '@mikro-orm/core';
+import logger from '../logger';
+import { Event } from '../entities/Event';
+
+export const events = Router();
+
+events.get('/', async (req, res) => {
+  try {
+    const availableEvents = await req.entityManager.find(Event, { start: QueryOrder.ASC });
+
+    // Check if user exists
+    if (!availableEvents) {
+      res.sendStatus(404);
+      return;
+    }
+
+    // Return stripped user information
+    res.status(200).send(availableEvents);
+  } catch (error) {
+    logger.error(`There was an issue getting events`, error);
+    res.status(500).send(`There was an issue getting events`);
+  }
+});
--- packages/api/src/api/index.ts
+++ packages/api/src/api/index.ts
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 import { Router } from 'express';
 import { auth } from './auth';
+import { events } from './events';
 import { health } from './health';
 import { users } from './users';
 import { videos } from './videos';
@@ -11,3 +12,4 @@ api.use('/health', health);
 api.use('/users', users);
 api.use('/videos', videos);
 api.use('/auth', auth);
+api.use('/event', events);
