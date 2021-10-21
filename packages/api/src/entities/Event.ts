80beec70c068dea8736aef99014777c53c745afb
Demondre Livingston <livingstondemondre7@gmail.com>
2021-10-21 13:53:16 -0400

    updating start and end date type

--- packages/api/src/entities/Event.ts
+++ packages/api/src/entities/Event.ts
@@ -10,16 +10,16 @@ export class Event extends Node<Event> {
   @Property({ columnType: 'text' })
   name: string;
 
-  @Property({ columnType: 'datetime' })
+  @Property({ columnType: 'timestamptz' })
   start: Date;
 
-  @Property({ columnType: 'datetime' })
+  @Property({ columnType: 'timestamptz' })
   end: Date;
 
   @Property({ columnType: 'text', nullable: true })
   description?: string;
 
-  constructor({ name, start, end, description,...extraValues }: EventConstructorValues) {
+  constructor({ name, start, end, description, ...extraValues }: EventConstructorValues) {
     super(extraValues);
 
     this.name = name;
--- packages/api/src/migrations/0003-add-the-event-table.ts
+++ packages/api/src/migrations/0003-add-the-event-table.ts
@@ -1,9 +1,11 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20211021161806 extends Migration {
-
+export class Migration20211021175229 extends Migration {
   async up(): Promise<void> {
-    this.addSql('create table "event" ("id" bigserial primary key, "createdAt" timestamptz(0) not null default clock_timestamp(), "updatedAt" timestamptz(0) not null default clock_timestamp(), "name" text not null, "start" date not null, "end" date not null, "description" text null);');
+    this.addSql('create table "event" ("id" bigserial primary key, "createdAt" timestamptz(0) not null default clock_timestamp(), "updatedAt" timestamptz(0) not null default clock_timestamp(), "name" text not null, "start" timestamptz not null, "end" timestamptz not null, "description" text null);');
   }
-
+  
+  async down(): Promise<void> {
+    this.addSql('drop table "event";');
+  }
 }
