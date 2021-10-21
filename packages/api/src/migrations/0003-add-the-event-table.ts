f82797b7bcf66efb2d5597e8ffe7254b3abe7896
Demondre Livingston <livingstondemondre7@gmail.com>
2021-10-21 14:55:16 -0400

    using default datetime option for event start/end time

--- packages/api/src/migrations/0003-add-the-event-table.ts
+++ packages/api/src/migrations/0003-add-the-event-table.ts
@@ -1,10 +1,10 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20211021175229 extends Migration {
+export class Migration20211021185343 extends Migration {
   async up(): Promise<void> {
-    this.addSql('create table "event" ("id" bigserial primary key, "createdAt" timestamptz(0) not null default clock_timestamp(), "updatedAt" timestamptz(0) not null default clock_timestamp(), "name" text not null, "start" timestamptz not null, "end" timestamptz not null, "description" text null);');
+    this.addSql('create table "event" ("id" bigserial primary key, "createdAt" timestamptz(0) not null default clock_timestamp(), "updatedAt" timestamptz(0) not null default clock_timestamp(), "name" text not null, "start" timestamptz(0) not null, "end" timestamptz(0) not null, "description" text null);');
   }
-  
+
   async down(): Promise<void> {
     this.addSql('drop table "event";');
   }
