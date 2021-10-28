1caf8c3e7cc92993b8f589d3d72ad03a475425af
Demondre Livingston <livingstondemondre7@gmail.com>
2021-10-28 18:02:00 -0400

    prettier

--- packages/web/src/components/Schedule/ScheduleRow.tsx
+++ packages/web/src/components/Schedule/ScheduleRow.tsx
@@ -29,7 +29,7 @@ const formatDate = (isoDate: string) => {
   const luxonDate = DateTime.fromISO(isoDate);
   const localDate = luxonDate.toFormat('ccc, LLL d, t'); // '2019/10/10'
   const relativeDate = luxonDate.toRelative() ?? ' '; // 'in 2 days'
- // console.log(isoDate, luxonDate.toRelative());
+  // console.log(isoDate, luxonDate.toRelative());
   return (
     <VStack alignItems="flex-start">
       <Text>{localDate}</Text>
