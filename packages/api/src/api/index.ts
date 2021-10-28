c022751abe843012645a085fac58d092ef0746ea
Demondre Livingston <livingstondemondre7@gmail.com>
2021-10-28 12:28:02 -0400

    Events endpoint (#402)
    * adding event entity to db
    
    * updating contact in Authors
    
    * removing unneeded gitIgnore
    
    * Apply suggestions from code review
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
    
    * initial commit for adding the event api endpoint
    
    * updating start and end date type
    
    * using default datetime option for event start/end time
    
    * adding tests to events endpoint
    
    * adding suggested changes
    
    * adding more suggested changes
    
    * prettier
    
    * removing unneeded tests
    
    * adding ignore to Prize entity for test coverage
    
    * Apply suggestions from code review
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
    
    * fixing tests
    
    * fixing sorting changes
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>

--- packages/api/src/api/index.ts
+++ packages/api/src/api/index.ts
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 import { Router } from 'express';
 import { auth } from './auth';
+import { events } from './events';
 import { health } from './health';
 import { users } from './users';
 
@@ -9,3 +10,4 @@ export const api = Router();
 api.use('/health', health);
 api.use('/users', users);
 api.use('/auth', auth);
+api.use('/event', events);
