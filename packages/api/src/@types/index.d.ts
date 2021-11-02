b1a80780f0b0cdf95032dd820f54e735074db5b8
Demondre Livingston <livingstondemondre7@gmail.com>
2021-11-02 16:09:10 -0400

    prettier

--- packages/api/src/@types/index.d.ts
+++ packages/api/src/@types/index.d.ts
@@ -2,7 +2,6 @@ import type { EntityManager } from '@mikro-orm/core';
 import type { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import type { User as UserEntity } from '../entities/User';
 
-
 declare global {
   namespace Express {
     export interface Request {
