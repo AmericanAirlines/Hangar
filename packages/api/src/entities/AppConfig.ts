53a8f8c47067f52dce324229ef79cb780062bae0
Austin Slobodnik <austinslobodnik@gmail.com>
2021-11-11 17:06:53 -0600

    getValueAsImplementation

--- packages/api/src/entities/AppConfig.ts
+++ packages/api/src/entities/AppConfig.ts
@@ -1,10 +1,11 @@
 /* istanbul ignore file */
 import { Entity, Property } from '@mikro-orm/core';
+import { EntityManager } from '@mikro-orm/knex';
 import { initDatabase } from '../database';
 import { ConstructorValues } from '../utils/types';
 import { Node } from './Node';
 
-export enum ConfigKey {};
+export enum ConfigKey {}
 
 type JSONValue = string | number | boolean | Record<string, any> | any[];
 
@@ -30,91 +31,144 @@ export class AppConfig extends Node<AppConfig> {
     return ['key', 'value'];
   }
 
-   /**
+  /**
    * Get the value of a Config item that is a string, and return null if it's not
    */
-    static async getValueAs(key: ConfigKey, valueType: 'string', shouldThrow: false): Promise<string | null>;
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'string',
+    shouldThrow: false,
+  ): Promise<string | null>;
 
-    /**
-     * Get the value of a Config item that is a string, and throw an error if it's not
-     */
-    static async getValueAs(key: ConfigKey, valueType: 'string', shouldThrow: true): Promise<string>;
-  
-    /**
-     * Get the value of a Config item that is a boolean, and return null if it's not
-     */
-    static async getValueAs(key: ConfigKey, valueType: 'boolean', shouldThrow: false): Promise<boolean | null>;
-  
-    /**
-     * Get the value of a Config item that is a boolean, and throw an error if it's not
-     */
-    static async getValueAs(key: ConfigKey, valueType: 'boolean', shouldThrow: true): Promise<boolean>;
-  
-    /**
-     * Get the value of a Config item that is a number, and return null if it's not
-     */
-    static async getValueAs(key: ConfigKey, valueType: 'number', shouldThrow: false): Promise<number | null>;
-  
-    /**
-     * Get the value of a Config item that is a number, and throw an error if it's not
-     */
-    static async getValueAs(key: ConfigKey, valueType: 'number', shouldThrow: true): Promise<number>;
-  
-    /**
-     * Get the value of a Config item that is a object, and return null if it's not
-     */
-    static async getValueAs(key: ConfigKey, valueType: 'object', shouldThrow: false): Promise<Record<string, any> | null>;
-  
-    /**
-     * Get the value of a Config item that is a object, and throw an error if it's not
-     */
-    static async getValueAs(key: ConfigKey, valueType: 'object', shouldThrow: true): Promise<Record<string, any>>;
-  
-    /**
-     * Get the value of a Config item that is a array, and return null if it's not
-     */
-    static async getValueAs(key: ConfigKey, valueType: 'array', shouldThrow: false): Promise<any[] | null>;
-  
-    /**
-     * Get the value of a Config item that is a array, and throw an error if it's not
-     */
-    static async getValueAs(key: ConfigKey, valueType: 'array', shouldThrow: true): Promise<any[]>;
-  
-    static async getValueAs(
-      key: ConfigKey,
-      valueType: 'string' | 'boolean' | 'number' | 'object' | 'array',
-      shouldThrow: boolean,
-    ): Promise<string | boolean | number | Record<string, any> | null> {
-      try {
-        const item = await ; // fix
-  
-        if (!item) {
-          if (shouldThrow) {
-            throw new Error(`No config item with the key ${key}`);
-          } else {
-            return null;
-          }
-        }
-  
-        if (item.value === null && shouldThrow) {
-          throw new Error('Value was null');
-        }
-  
-        if (valueType === 'array' && !Array.isArray(item.value)) {
-          throw new Error(`Value is not an array. It is of type ${typeof item.value}`);
-        }
-  
-        if (valueType !== 'array' && typeof item.value !== valueType) {
-          throw new Error(`Value was of undesired type. Requested type was ${valueType} but found ${typeof item.value}.`);
-        }
-  
-        return item.value;
-      } catch (err) {
+  /**
+   * Get the value of a Config item that is a string, and throw an error if it's not
+   */
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'string',
+    shouldThrow: true,
+  ): Promise<string>;
+
+  /**
+   * Get the value of a Config item that is a boolean, and return null if it's not
+   */
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'boolean',
+    shouldThrow: false,
+  ): Promise<boolean | null>;
+
+  /**
+   * Get the value of a Config item that is a boolean, and throw an error if it's not
+   */
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'boolean',
+    shouldThrow: true,
+  ): Promise<boolean>;
+
+  /**
+   * Get the value of a Config item that is a number, and return null if it's not
+   */
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'number',
+    shouldThrow: false,
+  ): Promise<number | null>;
+
+  /**
+   * Get the value of a Config item that is a number, and throw an error if it's not
+   */
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'number',
+    shouldThrow: true,
+  ): Promise<number>;
+
+  /**
+   * Get the value of a Config item that is a object, and return null if it's not
+   */
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'object',
+    shouldThrow: false,
+  ): Promise<Record<string, any> | null>;
+
+  /**
+   * Get the value of a Config item that is a object, and throw an error if it's not
+   */
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'object',
+    shouldThrow: true,
+  ): Promise<Record<string, any>>;
+
+  /**
+   * Get the value of a Config item that is a array, and return null if it's not
+   */
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'array',
+    shouldThrow: false,
+  ): Promise<any[] | null>;
+
+  /**
+   * Get the value of a Config item that is a array, and throw an error if it's not
+   */
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'array',
+    shouldThrow: true,
+  ): Promise<any[]>;
+
+  static async getValueAs(
+    em: EntityManager,
+    key: ConfigKey,
+    valueType: 'string' | 'boolean' | 'number' | 'object' | 'array',
+    shouldThrow: boolean,
+  ): Promise<string | boolean | number | Record<string, any> | null> {
+    try {
+      const item = await em.findOne(AppConfig, { key });
+
+      if (!item) {
         if (shouldThrow) {
-          throw err;
+          throw new Error(`No config item with the key ${key}`);
         } else {
           return null;
         }
       }
+
+      if (item.value === null && shouldThrow) {
+        throw new Error('Value was null');
+      }
+
+      if (valueType === 'array' && !Array.isArray(item.value)) {
+        throw new Error(`Value is not an array. It is of type ${typeof item.value}`);
+      }
+
+      if (valueType !== 'array' && typeof item.value !== valueType) {
+        throw new Error(
+          `Value was of undesired type. Requested type was ${valueType} but found ${typeof item.value}.`,
+        );
+      }
+
+      return item.value;
+    } catch (err) {
+      if (shouldThrow) {
+        throw err;
+      } else {
+        return null;
+      }
     }
+  }
 }
