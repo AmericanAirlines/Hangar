bce2f22a74fc5b90c158e570cf5cec316d1d3225
Austin Slobodnik <austinslobodnik@gmail.com>
2021-11-12 18:41:01 -0600

    minorFixes

--- packages/api/src/api/queue.ts
+++ packages/api/src/api/queue.ts
@@ -40,14 +40,19 @@ queue.post('/join', async (req, res) => {
   }
 
   try {
-    const currentQueueItems = await req.entityManager.find(QueueUser, { user, status: { $in: ['Pending', 'InProgress'] }});
-    if(currentQueueItems.length !== 0) {
+    const currentQueueItems = await req.entityManager.find(QueueUser, {
+      user,
+      status: { $in: ['Pending', 'InProgress'] },
+    });
+    if (currentQueueItems.length !== 0) {
       res.status(409).send('It appears that you are already waiting in a queue!');
+      return;
     }
   } catch (err) {
     const errorMsg = 'There was an issue with checking if the user is already in a queue';
     logger.error(`${errorMsg}: `, err);
     res.status(500).send(errorMsg);
+    return;
   }
 
   try {
