113f5d1488941ccf54ded50c0c2ef7516eca522a
Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
2023-07-28 12:51:47 -0500

    feat: reducing max line length to 100, removing stale plugin recommendation, fixing workflow triggers (#471)
    * feat: reducing max line length to 100, removing stale plugin recommendation
    
    * Fixing workflows
    
    * Fixing more workflows
    
    * Fixing another issue with typechecking
    
    * Fixing lint errors

---
+++ packages/web/src/components/ErrorPageContent/catGifs.ts
@@ -0,0 +1,9 @@
+export const catGifs: string[] = [
+  'https://media3.giphy.com/media/mlvseq9yvZhba/giphy.gif',
+  'https://media3.giphy.com/media/13CoXDiaCcCoyk/giphy.gif',
+  'https://media1.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif',
+  'https://media0.giphy.com/media/lJNoBCvQYp7nq/giphy.gif',
+  'https://media4.giphy.com/media/xTiQygY6HW1GjoYKFq/giphy.gif',
+  'https://media3.giphy.com/media/bPWyTsy2huZji/giphy.gif',
+  'https://media2.giphy.com/media/12bjQ7uASAaCKk/giphy.gif',
+];
