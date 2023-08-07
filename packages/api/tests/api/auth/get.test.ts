dee32cafae1bac035fbe7be379bd6c1ddbf23543
Grant Hill <grant.hill1@aa.com>
2023-08-07 13:56:34 -0500

    fix a few messed up endpoints

--- packages/api/tests/api/auth/get.test.ts
+++ packages/api/tests/api/auth/get.test.ts
@@ -3,7 +3,7 @@ import { get } from '../../../src/api/auth/get';
 describe('auth SLACK', () => {
   it('redirects to correct url for happy path', async () => {
     const fullLink =
-      'https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&redirect_uri=undefined%2Fapi%2Fauth%2Fcallback%2Fget&client_id=undefined';
+      'https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&redirect_uri=undefined%2Fapi%2Fauth%2Fcallback%2Fslack%2F&client_id=undefined';
 
     const mockRedirect = jest.fn();
     const mockReq = {};
