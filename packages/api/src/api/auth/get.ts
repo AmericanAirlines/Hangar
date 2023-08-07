05c8cab72cc695bb1a12df01703d3c9122ab20a9
granthill7 <51276876+granthill7@users.noreply.github.com>
2023-08-07 15:35:04 -0500

    feat: fix a few messed up endpoints (#479)
    * fix a few messed up endpoints
    
    * fix test per request
    
    * fix UI button route

--- packages/api/src/api/auth/get.ts
+++ packages/api/src/api/auth/get.ts
@@ -4,7 +4,7 @@ import { env } from '../../env';
 const slackAuthBaseUrl: string =
   'https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&response_type=code&';
 const queryArgs = new URLSearchParams({
-  redirect_uri: `${env.baseUrl}/api/auth/callback/get`,
+  redirect_uri: `${env.baseUrl}/api/auth/callback/slack/`,
   client_id: env.slackClientID,
 });
 const fullLink = `${slackAuthBaseUrl}${queryArgs.toString()}`;
