5c78eee2374f4a239b704585a3b24e716e94a8ae
Spencer Kaiser <spencer.kaiser@aa.com>
2023-08-08 18:19:59 -0500

    Updating db

---
+++ packages/database/src/migrations/0002-add-email-to-user-table.ts
@@ -0,0 +1,15 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20230808231944 extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('alter table "User" add column "email" text not null;');
+    this.addSql('alter table "User" add constraint "User_email_unique" unique ("email");');
+  }
+
+  async down(): Promise<void> {
+    this.addSql('alter table "User" drop constraint "User_email_unique";');
+    this.addSql('alter table "User" drop column "email";');
+  }
+
+}
