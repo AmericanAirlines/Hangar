26cfd716ee20eb938f9814d392a4df7f376ddb55
Saraah Cooper <33944755+EatMoreOranges@users.noreply.github.com>
2023-08-08 16:45:17 -0500

    feat: Update Branding (#477)
    * updated branding colors
    
    * responsive nav bar with the links to new font(font not visible yet)
    
    * update authors + css for navbar
    
    * Delete package-lock.json
    
    * updated packages based on pr review
    
    * updated with pr changes
    
    * fixed package mix up in root and workspace 😅
    
    * adding yarn.lock
    
    * fixed linting issue
    
    * Grant - changed heading, added routes to navbar
    
    * added darkness for dark mode
    
    * PR changes
    
    * update package.json
    
    * removed unused font package
    
    ---------
    
    Co-authored-by: Grant Hill <grant.hill1@aa.com>

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,24 +1,11 @@
 import { NextPage } from 'next';
-import { Button, Flex } from '@chakra-ui/react';
 import { appName } from '@hangar/shared';
 import React from 'react';
 import { PageContainer } from '../components/layout/PageContainer';
 
-async function signInWithSlack() {
-  window.open('/api/auth/');
-}
-
 const Home: NextPage = () => (
-  <PageContainer pageTitle={appName} heading={'Hello world'}>
+  <PageContainer pageTitle={appName} heading={'Welcome to Hangar!'}>
     {/* cspell:disable-next */}
-    <Flex>lorem ipsum dolor sit amet</Flex>
-    <Button
-      onClick={async () => {
-        await signInWithSlack();
-      }}
-    >
-      Sign in with Slack
-    </Button>
   </PageContainer>
 );
 
