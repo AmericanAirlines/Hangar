38dd17aea5609c60c7ad88cbaf3b41de522b6770
Alexander Allen <alexander.allen@aa.com>
2023-08-08 16:23:15 -0500

    fix Prize  property

--- packages/database/src/entities/Prize.ts
+++ packages/database/src/entities/Prize.ts
@@ -9,21 +9,22 @@ export type PrizeConstructorValues = ConstructorValues<Prize, never, 'isBonus'>;
 export class Prize extends Node<Prize> {
   @Property({ columnType: 'text' })
   name: string;
-
+  
   @Property({ columnType: 'int', unique: true })
   position: number;
-
+  
   @Property({ columnType: 'text', nullable: true })
   description?: string;
-
+  
   @Property({ columnType: 'boolean' })
   isBonus: boolean;
-
+  
   constructor({ name, position, description, isBonus = false,  ...extraValues }: PrizeConstructorValues) {
     super(extraValues);
-
+    
     this.name = name;
-    this.sortOrder = sortOrder;
+    this.position = position;
+    this.isBonus = isBonus;
   }
 
 }
