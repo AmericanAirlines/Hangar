8edd6ba0d1abc7ae2d3abb37fd9ed08a2fa0dee6
Saraah Cooper <saraah.cooper@aa.com>
2023-08-08 09:07:54 -0500

    fixed linting issue

--- packages/web/src/components/layout/AppLayout/AppLayout.tsx
+++ packages/web/src/components/layout/AppLayout/AppLayout.tsx
@@ -1,5 +1,4 @@
 import { Box, Center, ChakraProps, Flex } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
 import { colors } from '../../../theme';
 import { NavBar } from './NavBar/NavBar';
 
@@ -10,38 +9,34 @@ type AppLayoutProps = ChakraProps & {
 const MAX_CONTENT_WIDTH_IN_PIXELS = 1200;
 const NAV_HEIGHT = '60px';
 
-export const AppLayout: React.FC<AppLayoutProps> = ({ children, ...chakraProps }) => {
-  const router = useRouter();
+export const AppLayout: React.FC<AppLayoutProps> = ({ children, ...chakraProps }) => (
+  <>
+    <Box w="full" marginBottom={4} px={4} bgColor={colors.brandPrimary}>
+      <Flex
+        as="nav"
+        flexWrap="wrap"
+        justifyContent="space-between"
+        alignItems="center"
+        maxW={MAX_CONTENT_WIDTH_IN_PIXELS}
+        marginX="auto"
+        gap={0}
+        minH={NAV_HEIGHT}
+      >
+        {/* Add nav elements here */}
+        <NavBar />
+      </Flex>
+    </Box>
 
-  return (
-    <>
-      <Box w="full" marginBottom={4} px={4} bgColor={colors.brandPrimary}>
-        <Flex
-          as="nav"
-          flexWrap="wrap"
-          justifyContent="space-between"
-          alignItems="center"
-          maxW={MAX_CONTENT_WIDTH_IN_PIXELS}
-          marginX="auto"
-          gap={0}
-          minH={NAV_HEIGHT}
-        >
-          {/* Add nav elements here */}
-          <NavBar />
-        </Flex>
-      </Box>
-
-      <Center>
-        <Flex
-          width="100%"
-          maxWidth={`${MAX_CONTENT_WIDTH_IN_PIXELS}px`}
-          alignItems="stretch"
-          direction="column"
-          {...chakraProps}
-        >
-          {children}
-        </Flex>
-      </Center>
-    </>
-  );
-};
+    <Center>
+      <Flex
+        width="100%"
+        maxWidth={`${MAX_CONTENT_WIDTH_IN_PIXELS}px`}
+        alignItems="stretch"
+        direction="column"
+        {...chakraProps}
+      >
+        {children}
+      </Flex>
+    </Center>
+  </>
+);
