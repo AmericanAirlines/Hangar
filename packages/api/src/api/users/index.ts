d5638c34edb03c73f1ea0f2332dc4b3b5cf58890
Spencer Kaiser <spencer.kaiser@aa.com>
2023-08-16 13:33:22 -0500

    feat: splitting session middleware

--- packages/api/src/api/users/index.ts
+++ packages/api/src/api/users/index.ts
@@ -1,17 +1,7 @@
 import { Router } from 'express';
 import { post } from './post';
+import { sessionMiddleware } from '../../middleware/sessionMiddleware';
 
 export const users = Router();
 
-users.post(
-  '',
-  (req, res, next) => {
-    if (!req.session?.email) {
-      // User does not have a valid session
-      res.sendStatus(401);
-      return;
-    }
-    next();
-  },
-  post,
-);
+users.post('', sessionMiddleware, post);
