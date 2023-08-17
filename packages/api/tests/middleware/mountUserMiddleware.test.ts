64a9fe6afe2e0d10a96762e036269a30002d6af4
Grant Hill <grant.hill1@aa.com>
2023-08-17 15:24:17 -0500

    Ran prettier:fix

--- packages/api/tests/middleware/mountUserMiddleware.test.ts
+++ packages/api/tests/middleware/mountUserMiddleware.test.ts
@@ -6,52 +6,52 @@ describe('mounting user on /user', () => {
   it('adds a user to the request when a valid session is found', async () => {
     // setup
     const req = {
-      ...createMockReq() ,
-      session: { id: '1' }
+      ...createMockReq(),
+      session: { id: '1' },
     };
     const mockNext = jest.fn();
-    const mockRes = createMockRes()
-    
+    const mockRes = createMockRes();
+
     // test
     await mountUserMiddleware(req as unknown as Request, mockRes as unknown as Response, mockNext);
-    
+
     // assert
     expect(req.user).toBeDefined();
     expect(mockNext).toHaveBeenCalled();
   });
-  
+
   it('sends a 403 status when a valid session is found but the user is not authorized', async () => {
     // setup
     const req = {
-      ...createMockReq() ,
-      session: { id: undefined }
+      ...createMockReq(),
+      session: { id: undefined },
     };
     const mockNext = jest.fn();
-    const mockRes = createMockRes()
-    
+    const mockRes = createMockRes();
+
     // test
     await mountUserMiddleware(req as unknown as Request, mockRes as unknown as Response, mockNext);
-    
+
     // assert
     expect(mockRes.sendStatus).toHaveBeenCalledWith(403);
     expect(mockNext).not.toHaveBeenCalled();
   });
-  
+
   it('sends a 500 status when an error occurs', async () => {
     // setup
     const req = {
-      ...createMockReq() ,
-      session: { id: '1' }
+      ...createMockReq(),
+      session: { id: '1' },
     };
     req.entityManager.findOne = jest.fn(() => {
       throw new Error('test error');
     });
     const mockNext = jest.fn();
-    const mockRes = createMockRes()
-    
+    const mockRes = createMockRes();
+
     // test
     await mountUserMiddleware(req as unknown as Request, mockRes as unknown as Response, mockNext);
-    
+
     // assert
     expect(mockRes.sendStatus).toHaveBeenCalledWith(500);
     expect(mockNext).not.toHaveBeenCalled();
