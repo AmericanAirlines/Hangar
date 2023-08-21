f71d77e206d4ea894eb3a9acc56e4ccd5277c357
Grant Hill <grant.hill1@aa.com>
2023-08-21 16:38:58 -0500

    fixed store in second huddle

--- packages/web/src/stores/user.ts
+++ packages/web/src/stores/user.ts
@@ -1,26 +1,7 @@
-import { User } from '@hangar/shared';
+import { SerializedUser, User } from '@hangar/shared';
+import axios from 'axios';
+import dayjs from 'dayjs';
 import create from 'zustand';
-
-const mockFetchUser = () =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        json: () =>
-          new Promise((resolveJSON) => {
-            setTimeout(() => {
-              resolveJSON({
-                data: {
-                  id: 1,
-                  firstName: 'John',
-                  lastName: 'Doe',
-                  email: '',
-                },
-              });
-            }, 1000);
-          }),
-      });
-    }, 1000);
-  });
 
 type UserStore = {
   user?: User;
@@ -32,30 +13,18 @@ export const useUserStore = create<UserStore>((set) => ({
   user: undefined,
   doneLoading: false,
   fetchUser: async () => {
-    const res: any = await mockFetchUser();
-    // const res = await fetch(`http://localhost:3008/api/users/me`,{cache:'no-cache'})
-    const json = (await res.json()).data;
-    const payload = { user: json, doneLoading: true };
-    set(payload);
+    let user: User | undefined;
+    try {
+      const res = await axios.get<SerializedUser>(`/api/users/me`);
+      const { createdAt, updatedAt, ...rest } = res.data;
+      user = { ...rest, createdAt: dayjs(createdAt), updatedAt: dayjs(updatedAt) };
+    } catch (error) {
+      if (!axios.isAxiosError(error) || error.status !== 401) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+        // TODO: Show error toast
+      }
+    }
+    set({ user, doneLoading: true });
   },
-  // createUser: async (user) => {
-  // const res = await fetch(`http://localhost:3008/api/users`, {
-  //   cache: 'no-cache',
-  //   method: 'post',
-  //   headers: { 'content-type': 'application/json' },
-  //   body: JSON.stringify({ ...user })
-  // })
-  // const json = await res.json()
-  // set({ user: json.data })
-  // },
-  // updateUser: async (user) => {
-  // const res = await fetch(`http://localhost:3008/api/users/me`, {
-  //   cache: 'no-cache',
-  //   method: 'put',
-  //   headers: { 'content-type': 'application/json' },
-  //   body: JSON.stringify({ ...user })
-  // })
-  // const json = await res.json()
-  // set({ user: json.data })
-  // },
 }));
