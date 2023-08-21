5064140b9c16f9850d7ce5e769b886ef8118d292
Grant Hill <grant.hill1@aa.com>
2023-08-21 14:27:44 -0500

    Added to NavBar/NavDrawer

--- packages/web/src/components/layout/AppLayout/NavBar/NavDrawer.tsx
+++ packages/web/src/components/layout/AppLayout/NavBar/NavDrawer.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Drawer,
   DrawerBody,
@@ -7,41 +8,62 @@ import {
   DrawerOverlay,
   VStack,
 } from '@chakra-ui/react';
+import { useEffect } from 'react';
 import { signInWithSlack } from './NavBar';
+import { useUserStore } from '../../../../stores/user';
 
 type NavDrawerProps = {
   isOpen: any;
   onClose: any;
+  doneLoading: boolean;
 };
 
-export const NavDrawer: React.FC<NavDrawerProps> = (props) => (
-  <Drawer placement="left" size={'xs'} onClose={props.onClose} isOpen={props.isOpen}>
-    <DrawerOverlay />
-    <DrawerContent>
-      <DrawerBody>
-        <VStack>
-          <Button
-            onClick={async () => {
-              await signInWithSlack();
-            }}
-          >
-            Sign Up
-          </Button>
-          <Button
-            onClick={async () => {
-              await signInWithSlack();
-            }}
-          >
-            Login
-          </Button>
-        </VStack>
-      </DrawerBody>
+export const NavDrawer: React.FC<NavDrawerProps> = (props) => {
+  const { user, doneLoading, fetchUser } = useUserStore((state: any) => state);
 
-      <DrawerFooter>
-        <Button variant="outline" mr={3}>
-          Admin
-        </Button>
-      </DrawerFooter>
-    </DrawerContent>
-  </Drawer>
-);
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
+  return (
+    <Drawer placement="left" size={'xs'} onClose={props.onClose} isOpen={props.isOpen}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerBody>
+          {doneLoading ? (
+            <Box>
+              {user.firstName ? (
+                <Box alignContent={'center'}>Welcome back, {user.firstName}!</Box>
+              ) : (
+                <VStack>
+                  <Button
+                    onClick={async () => {
+                      await signInWithSlack();
+                    }}
+                  >
+                    Sign Up
+                  </Button>
+                  <Button
+                    onClick={async () => {
+                      await signInWithSlack();
+                    }}
+                  >
+                    Login
+                  </Button>
+                </VStack>
+              )}
+            </Box>
+          ) : (
+            <Box>Loading...</Box>
+          )}
+        </DrawerBody>
+
+        <DrawerFooter>
+          <Button variant="outline" mr={3}>
+            Admin
+          </Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
