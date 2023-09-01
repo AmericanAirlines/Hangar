dd0374411ee03c0aeee47661b9d632e4f8c97a3f
Grant Hill <grant.hill1@aa.com>
2023-09-01 09:54:10 -0500

    Initial Commit

---
+++ packages/database/src/entities/JudgingSession.ts
@@ -0,0 +1,29 @@
+import { ManyToOne, Property, Entity, Ref } from '@mikro-orm/core';
+import { v4 } from 'uuid';
+import { Node } from './Node';
+import { ConstructorValues } from '../types/ConstructorValues';
+import { User } from './User';
+
+export type JudgingSessionConstructorValues = ConstructorValues<JudgingSession>;
+
+@Entity()
+export abstract class JudgingSession extends Node<JudgingSession> {
+  @Property()
+  inviteCode: string = v4();
+
+  @ManyToOne({ entity: () => User, nullable: false, ref: true })
+  createdBy: Ref<User>;
+
+  @Property()
+  createdAt: Date = new Date();
+
+  @Property({ onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+
+  constructor({ inviteCode, createdBy }: JudgingSessionConstructorValues) {
+    super();
+
+    this.inviteCode = inviteCode;
+    this.createdBy = createdBy;
+  }
+}
