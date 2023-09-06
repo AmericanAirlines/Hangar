b78ddb159d4807b582c2dbfa5935d005298fd38d
Grant Hill <grant.hill1@aa.com>
2023-09-06 15:57:28 -0500

    remove createdBy from schema

--- packages/shared/src/schema/judgingSession/post.ts
+++ packages/shared/src/schema/judgingSession/post.ts
@@ -2,9 +2,6 @@ import { z } from 'zod';
 
 const judgingSession = z.object({
   inviteCode: z.string(),
-  createdBy: z.object({
-    // What should this look like? It's a Ref<User> in database/src/entities/JudgingSession.ts
-  }),
 });
 
 export const put = z.object({
