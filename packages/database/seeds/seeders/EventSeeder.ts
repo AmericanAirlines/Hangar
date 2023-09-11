9211b1d4072b70cce8da27c53d67e438c461a386
Spencer Kaiser <spencer.kaiser@aa.com>
2023-09-11 10:18:11 -0500

    fixing and improving the event seeder

--- packages/database/seeds/seeders/EventSeeder.ts
+++ packages/database/seeds/seeders/EventSeeder.ts
@@ -4,20 +4,21 @@ import dayjs from 'dayjs';
 import { Seeder } from '@mikro-orm/seeder';
 import { EventFactory } from '../factories/EventFactory';
 
-const eventsToMake = 10;
+const eventsToMake = 20;
 
 export class EventSeeder extends Seeder {
   run = async (em: EntityManager): Promise<void> => {
     const eventFactory = new EventFactory(em);
 
-    let now = dayjs();
+    let now = dayjs().subtract(4, 'hours').startOf('hour');
 
     for (let i = 0; i < eventsToMake; i += 1) {
       const duration = Math.round(Math.random()) * 30 + 30; // 30 or 60 mins
-      const start = now.toDate();
-      now = now.add(duration, 'minutes');
-      const end = now.toDate();
-      em.persist(eventFactory.makeOne({ start, end }));
+      const randStartTimeShift = Math.random() > 0.8 ? 30 * Math.ceil(Math.random() * 5) : 0; // Random amount of time between 0 mins and 150 mins
+      const start = now.add(randStartTimeShift, 'minutes'); // Shift the start time by a random amount 20% of the time
+      const end = start.add(duration, 'minutes');
+      now = end;
+      em.persist(eventFactory.makeOne({ start: start.toDate(), end: end.toDate() }));
     }
   };
 }
