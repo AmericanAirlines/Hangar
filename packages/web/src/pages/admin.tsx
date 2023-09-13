cea5c6b8e7c1080051b186bb122fd6dac9db5304
Saraah Cooper <saraah.cooper@aa.com>
2023-09-13 09:27:08 -0500

    draft pr: created admin template page

---
+++ packages/web/src/pages/admin.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { NextPage } from 'next';
+import { PageContainer } from '../components/layout/PageContainer';
+
+const AdminDashboard: NextPage = () => (
+  <PageContainer pageTitle={'Admin Dashboard'} heading={'Admin Dashboard'}></PageContainer>
+);
