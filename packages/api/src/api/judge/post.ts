51312f49459008042bf397d94a606a359a6d0caf
Ansaar Syed <Ansaar.Syed@aa.com>
2023-09-20 08:52:06 -0500

    Added matching code functionality

--- packages/api/src/api/judge/post.ts
+++ packages/api/src/api/judge/post.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { Judge, User } from '@hangar/database';
+import { Judge, User, JudgingSession } from '@hangar/database';
 import { Schema } from '@hangar/shared';
 import { DriverException } from '@mikro-orm/core';
 import { logger } from '../../utils/logger';
@@ -15,6 +15,15 @@ export const post = async (req: Request, res: Response) => {
     data: { inviteCode },
   });
 
-  if (errorHandled) return;
-  // Incomplete
+  try {
+    const judgingSession = await req.entityManager.findOne(JudgingSession, {
+      inviteCode: data?.inviteCode,
+    });
+    if (data?.inviteCode === judgingSession?.inviteCode) {
+      res.sendStatus(200);
+    }
+  } catch {
+    res.sendStatus(403);
+  }
+  // Still need to add 2 more functionality
 };
