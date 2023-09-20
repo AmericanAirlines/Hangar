638bce9b5a9a18f8b54dc1a81fa9c7dc1b9d66e9
Alexander Allen <alexander.allen@aa.com>
2023-09-20 11:58:40 -0500

    first draft

--- packages/api/src/utils/authenticateUser.ts
+++ packages/api/src/utils/authenticateUser.ts
@@ -6,6 +6,7 @@ export type OAuthUserData = {
   email: string;
   firstName: string;
   lastName: string;
+  returnTo?: string;
 };
 
 type AuthenticateArgs = {
@@ -30,8 +31,12 @@ export const authenticateUser = async ({ data, req, res }: AuthenticateArgs) => 
       await em.persistAndFlush(newUser);
       req.session = { id: newUser.id };
     }
-
-    res.redirect('/');
+    if ( data?.returnTo ) {
+      res.redirect(data.returnTo);
+    }
+    else {
+      res.redirect('/');
+    }
   } catch (error) {
     logger.error(error);
     res.redirect('/error?description=Failed to resolve identity');
