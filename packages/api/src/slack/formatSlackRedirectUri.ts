c1cf2fcc65bcc42713efdc5953919be79406270e
Spencer Kaiser <spencer.kaiser@aa.com>
2023-09-21 17:42:29 -0500

    fix: fixing auth flow

---
+++ packages/api/src/slack/formatSlackRedirectUri.ts
@@ -0,0 +1,11 @@
+import { Config } from '@hangar/shared';
+import { env } from '../env';
+
+type FormatSlackRedirectUriArgs = {
+  returnTo?: string;
+};
+
+export const formatSlackRedirectUri = ({ returnTo }: FormatSlackRedirectUriArgs) => {
+  const returnToQuery = returnTo ? `?${Config.global.authReturnUriParamName}=${returnTo}` : '';
+  return `${env.baseUrl ?? ''}/api/auth/callback/slack${returnToQuery}`;
+};
