a36c65ce063bdd489a2b6cc657f1d2de34ff1104
Ansaar Syed <Ansaar.Syed@aa.com>
2023-09-25 13:22:47 -0500

    change the post and put schema for judge

--- packages/shared/src/schema/judge/post.ts
+++ packages/shared/src/schema/judge/post.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 import { commonSchema } from './common';
 
-export const post = z.object({
-  ...commonSchema.shape,
-});
+export const post = commonSchema.merge(
+  z.object({
+    // Add respective properties.
+  }),
+);
--- packages/shared/src/schema/judge/put.ts
+++ packages/shared/src/schema/judge/put.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 import { commonSchema } from './common';
 
-export const put = z.object({
-  ...commonSchema.shape,
-});
+export const put = commonSchema.merge(
+  z.object({
+    // Add respective properties.
+  }),
+);
