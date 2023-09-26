f58ae0d5377719bae511968bd95c635a8f9bb489
Alexander Allen <alexander.allen@aa.com>
2023-09-26 13:09:07 -0500

    schema tests

--- packages/shared/src/schema/project/contributors/put.ts
+++ packages/shared/src/schema/project/contributors/put.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const put = z.object({
-  projectId: z.string().uuid(),
-  inviteCode: z.string(),
+  projectId: z.string(),
+  inviteCode: z.string().uuid(),
 });
