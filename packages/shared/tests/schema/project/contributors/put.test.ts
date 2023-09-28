ec366c36ab0bf5135e27ecc1ece194d3fd8958cd
alexanderallen-aa <alexander.allen@aa.com>
2023-09-28 12:02:21 -0500

    feat: contributors put endpoint for projects (#535)
    * first draft
    
    * PR notes
    
    * schema tests
    
    * enpoint tests
    
    * migrations
    
    * linting
    
    * types
    
    * linting
    
    * Apply suggestions from code review
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
    
    * PR notes
    
    * Update put.test.ts
    
    ---------
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>

---
+++ packages/shared/tests/schema/project/contributors/put.test.ts
@@ -0,0 +1,31 @@
+import { v4 } from 'uuid';
+import { Schema } from '../../../../src';
+
+const validPut = {
+  projectId: '1',
+  inviteCode: v4(),
+};
+
+describe('project contributors put schema', () => {
+  it('validates a matching object', () => {
+    expect(Schema.project.contributors.put.safeParse(validPut).success).toBe(true);
+  });
+
+  it('does not validate an object with an invalid projectId', () => {
+    expect(
+      Schema.project.contributors.put.safeParse({
+        ...validPut,
+        projectId: 1,
+      }).success,
+    ).toBe(false);
+  });
+
+  it('does not validate an object with an invalid inviteCode', () => {
+    expect(
+      Schema.project.contributors.put.safeParse({
+        ...validPut,
+        inviteCode: `${v4()}-junk`,
+      }).success,
+    ).toBe(false);
+  });
+});
