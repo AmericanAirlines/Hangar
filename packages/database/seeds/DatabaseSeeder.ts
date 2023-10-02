4b0b1f0bd1ce6e78fc375b8b8e946376378a7f8b
Spencer Kaiser <spencer.kaiser@aa.com>
2023-10-02 14:28:08 -0500

    Adding judge to seeder

--- packages/database/seeds/DatabaseSeeder.ts
+++ packages/database/seeds/DatabaseSeeder.ts
@@ -4,8 +4,9 @@ import { UserSeeder } from './seeders/UserSeeder';
 import { AdminSeeder } from './seeders/AdminSeeder';
 import { EventSeeder } from './seeders/EventSeeder';
 import { PrizeSeeder } from './seeders/PrizeSeeder';
+import { JudgeSeeder } from './seeders/JudgeSeeder';
 
 export class DatabaseSeeder extends Seeder {
   run = async (em: EntityManager): Promise<any> =>
-    this.call(em, [UserSeeder, AdminSeeder, EventSeeder, PrizeSeeder]);
+    this.call(em, [UserSeeder, AdminSeeder, JudgeSeeder, EventSeeder, PrizeSeeder]);
 }
