6acd271b17a3a95b34acb2d3d1e2664451d25d2c
Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
2023-10-02 16:19:05 -0500

    feat: adding Expo Judging Session GET endpoint (#543)
    * feat: adding Expo Judging Session GET endpoint
    
    * ignoring a word
    
    * Adding test coverage
    
    * Small fixes

---
+++ packages/api/src/api/expoJudgingSession/id/get.ts
@@ -0,0 +1,34 @@
+import { ExpoJudgingSession } from '@hangar/database';
+import { Request, Response } from 'express';
+import { logger } from '../../../utils/logger';
+
+export const get = async (req: Request, res: Response) => {
+  const {
+    judge,
+    entityManager: em,
+    params: { id: ejsId },
+  } = req;
+
+  try {
+    const ejs = await em.findOne(ExpoJudgingSession, { id: ejsId as string });
+    if (!ejs) {
+      res.sendStatus(404);
+      return;
+    }
+
+    await em.populate(judge, ['expoJudgingSessionContexts']);
+    const hasAccess = judge.expoJudgingSessionContexts
+      .getItems()
+      .some((ejsc) => ejsc.expoJudgingSession.id === ejsId);
+
+    if (!hasAccess) {
+      res.sendStatus(403);
+      return;
+    }
+
+    res.send(ejs);
+  } catch (error) {
+    logger.error('Failed to query Expo Judging Session', error);
+    res.sendStatus(500);
+  }
+};
