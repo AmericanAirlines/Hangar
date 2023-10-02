e9ad58e3bd92120094565c04724b08e567b0ceac
Spencer Kaiser <spencer.kaiser@aa.com>
2023-10-02 14:26:36 -0500

    feat: adding Expo Judging Session GET endpoint

--- packages/api/src/api/expoJudgingSession/index.ts
+++ packages/api/src/api/expoJudgingSession/index.ts
@@ -2,8 +2,11 @@ import { Router } from 'express';
 import { post } from './post';
 import { get } from './get';
 import { adminMiddleware } from '../../middleware/adminMiddleware';
+import { judgeMiddleware } from '../../middleware/judgeMiddleware';
+import { id } from './id';
 
 export const expoJudgingSession = Router();
 
 expoJudgingSession.post('', adminMiddleware, post);
 expoJudgingSession.get('', adminMiddleware, get);
+expoJudgingSession.use('/:id', judgeMiddleware, id);
--- packages/api/src/middleware/judgeMiddleware.ts
+++ packages/api/src/middleware/judgeMiddleware.ts
@@ -1,19 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
 import { Judge } from '@hangar/database';
+import { logger } from '../utils/logger';
 
 /**
  * Middleware that evaluates the judge's validity
  *
  * Paths:
  *  - Next function invoked: judge was identified and matching object mounted to request
- *  - 400: Judge not found
+ *  - 401: Valid Session not found
+ *  - 403: Judge not found
  *  - 500: Error occurred trying to identify
  */
 
 export const judgeMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   let judge;
+
+  if (!req.session?.id) {
+    res.sendStatus(401);
+    return;
+  }
+
   try {
-    judge = await req.entityManager.findOne(Judge, { user: req.session?.id });
+    judge = await req.entityManager.findOne(Judge, { user: req.session.id });
   } catch {
     res.sendStatus(500);
     return;
@@ -21,7 +29,7 @@ export const judgeMiddleware = async (req: Request, res: Response, next: NextFun
 
   if (!judge) {
     // Judge does not exits in database
-
+    logger.debug(`JudgeMiddleware failed to authenticate user - ${req.originalUrl}`);
     res.sendStatus(403);
     return;
   }
