da12991b22a1023be853b22fb9f7c644468a7dab
Ansaar Syed <Ansaar.Syed@aa.com>
2023-10-04 13:53:36 -0500

    added the test case

---
+++ packages/shared/tests/utils/wait.test.ts
@@ -0,0 +1,16 @@
+import { wait } from '../../src/utils/wait';
+
+describe('wait function', () => {
+  it('waits for specified time', async () => {
+    const delay = 1000;
+    const mockFunc = jest.fn();
+
+    const waitPromise = wait(delay).then(mockFunc);
+
+    jest.advanceTimersByTime(delay);
+
+    await waitPromise;
+
+    expect(mockFunc).toHaveBeenCalled();
+  });
+});
