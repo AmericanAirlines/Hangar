26fac0f40a9f76caaff2dc6a2637e4e07d237794
Spencer Kaiser <spencer.kaiser@aa.com>
2023-10-04 12:20:56 -0500

    Refactoring button to use shared toast

--- packages/web/src/components/AddExpoJudgingSessionButton/AddExpoJudgingSessionButton.tsx
+++ packages/web/src/components/AddExpoJudgingSessionButton/AddExpoJudgingSessionButton.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import { Button, UseToastOptions, useToast } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 import { ExpoJudgingSession } from '@hangar/shared';
-
-const SUCCESS_POST_TOAST: UseToastOptions = {
-  title: 'Success!',
-  description: 'Expo Judging Session successfully created.',
-  status: 'success',
-  duration: 3000,
-  isClosable: true,
-};
-
-const FAILED_POST_TOAST: UseToastOptions = {
-  title: 'An error occurred.',
-  description: 'Unable to create a new Expo Judging Session.',
-  status: 'error',
-  duration: 3000,
-  isClosable: true,
-};
+import { openErrorToast, openSuccessToast } from '../utils/CustomToast';
 
 const postExpoJudgingSession: () => Promise<ExpoJudgingSession> = async () => {
   const res: ExpoJudgingSession = await axios.post('/api/expoJudgingSession');
@@ -25,15 +10,20 @@ const postExpoJudgingSession: () => Promise<ExpoJudgingSession> = async () => {
 };
 
 export const AddExpoJudgingSession: React.FC = () => {
-  const toast = useToast();
   let expoJudgingSession: ExpoJudgingSession | undefined;
 
   const tryPostExpoJudgingSession = async () => {
     try {
       expoJudgingSession = await postExpoJudgingSession();
-      toast(SUCCESS_POST_TOAST);
+      openSuccessToast({
+        title: 'Success!',
+        description: 'Expo Judging Session successfully created.',
+      });
     } catch {
-      toast(FAILED_POST_TOAST);
+      openErrorToast({
+        title: 'An error occurred.',
+        description: 'Unable to create a new Expo Judging Session.',
+      });
     }
 
     return expoJudgingSession;
