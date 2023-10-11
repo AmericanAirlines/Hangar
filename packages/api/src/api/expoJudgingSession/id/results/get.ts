e4bcc970e99cdb106758e57d78d2281e6f409f9d
Ansaar Syed <Ansaar.Syed@aa.com>
2023-10-11 11:39:36 -0500

    get end point

--- packages/api/src/api/expoJudgingSession/id/results/get.ts
+++ packages/api/src/api/expoJudgingSession/id/results/get.ts
@@ -6,17 +6,34 @@ export const get = async (req: Request, res: Response) => {
   const {
     entityManager: em,
     params: { id: ejsId },
-    user,
-    judge
+    judge,
   } = req;
 
   try {
-    const ejs = await em.findOne(ExpoJudgingSession, {id: ejsId as string})
-    if(!ejs){
-        res.sendStatus(404);
-        return:
+    const ejs = await em.findOne(ExpoJudgingSession, { id: ejsId as string });
+    if (!ejs) {
+      res.sendStatus(404);
+      return;
     }
 
-    await em.populate(judge, ['expoJudgingSessionContexts'])
+    await em.populate(judge, ['expoJudgingSessionContexts']);
+    const hasAccess = judge.expoJudgingSessionContexts
+      .getItems()
+      .some((ejsc) => ejsc.expoJudgingSession.id === ejsId);
+
+    if (!hasAccess) {
+      res.sendStatus(403);
+      return;
+    }
+
+    // before calling the tabulate method we need to get all the votes for the judging session.
+    const allVotes = await em.find(ExpoJudgingVote, { id: ejsId as string });
+
+    // still need to call tabulate method on the votes fetched for the judging session
+    const results = await ExpoJudgingVote.tabulate({ entityManager: em, expoJudgingSession: ejs });
+    res.send(results);
+  } catch (error) {
+    logger.error('Failed to fetch all votes', error);
+    res.sendStatus(500);
   }
 };
