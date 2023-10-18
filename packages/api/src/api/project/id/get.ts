a4da32c94ce8d3bfdb5612416f9362a25f2182d2
Ansaar Syed <Ansaar.Syed@aa.com>
2023-10-18 15:24:02 -0500

    fixed get endpoint

--- packages/api/src/api/project/id/get.ts
+++ packages/api/src/api/project/id/get.ts
@@ -8,11 +8,12 @@ export const get = async (req: Request, res: Response) => {
     params: { id: projectId },
   } = req;
   try {
-    const project = await entityManager.findOne(Project, { id: projectId as string });
+    const project = await entityManager.find(Project, { id: projectId as string });
     if (!project) {
       res.sendStatus(404);
     }
-    res.send(project?.id);
+    res.send(project);
+    console.log('hepg', project);
   } catch (error) {
     logger.error('Unable to fetch project details from dB', error);
     res.sendStatus(500);
