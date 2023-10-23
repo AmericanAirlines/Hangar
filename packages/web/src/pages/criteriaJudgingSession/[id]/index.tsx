48de82681195fd76bf04904550ccddb6a102643b
Spencer Kaiser <spencer.kaiser@aa.com>
2023-10-23 18:33:28 -0500

    feat: adding a project select to the criteria judging page

--- packages/web/src/pages/criteriaJudgingSession/[id]/index.tsx
+++ packages/web/src/pages/criteriaJudgingSession/[id]/index.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { Select } from '@chakra-ui/react';
 import { PageContainer } from '../../../components/layout/PageContainer';
 import { useJudgingSessionFetcher } from '../../../pageUtils/judgingSession';
+import { useCriteriaJudging } from '../../../components/CriteriaJudging/hooks/useCriteriaJudging';
 
 const CriteriaJudgingSessionDetails: NextPage = () => {
   const { criteriaJudgingSession } = useJudgingSessionFetcher({ sessionType: 'criteria' });
+  const { projects, isLoading } = useCriteriaJudging();
+
+  React.useEffect(() => {
+    if (criteriaJudgingSession) {
+      useCriteriaJudging.getState().init({ criteriaJudgingSessionId: criteriaJudgingSession.id });
+    }
+  }, [criteriaJudgingSession]);
 
   return (
     <PageContainer
       pageTitle={'Criteria Judging'}
       heading={'Criteria Judging'}
-      isLoading={!criteriaJudgingSession}
+      isLoading={!criteriaJudgingSession || isLoading}
     >
-      TBD
+      <Select placeholder="Select a Project">
+        {projects?.map((project) => (
+          <option key={project.id} value={project.name}>
+            {project.name}
+          </option>
+        ))}
+      </Select>
     </PageContainer>
   );
 };
