2ef1f20ff6876cf7d2e9bbecd2f7e8d51c3d46d5
Saraah Cooper <saraah.cooper@aa.com>
2023-10-23 10:15:22 -0500

    intro text updated

--- packages/web/src/pages/judgingIntro.tsx
+++ packages/web/src/pages/judgingIntro.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Box, Text, Button, Flex, List, ListItem, ListIcon, Icon } from '@chakra-ui/react';
+import { Box, Button, Flex, List, ListItem, ListIcon } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { MdCheckCircle, MdSettings } from 'react-icons/md';
-import { BsFill1SquareFill, BsFill2SquareFill, BsFill3SquareFill } from 'react-icons/bs';
+import { BsFill1CircleFill, BsFill2CircleFill, BsFill3CircleFill } from 'react-icons/bs';
 import { PageContainer } from '../components/layout/PageContainer';
 import { useExpoJudgingSessionFetcher } from '../pageUtils/expoJudgingSession/[id]/useExpoJudgingSessionFetcher';
 
@@ -17,26 +16,24 @@ const JudgingIntro = () => {
   return (
     <PageContainer
       pageTitle={'Judging'}
-      heading={'How Judging Works'}
+      heading={'How Expo Judging Works'}
       isLoading={!expoJudgingSession}
     >
       <Flex direction="column" gap={5}>
         <Box>
           <List spacing={4}>
             <ListItem fontSize={20}>
-              <ListIcon as={BsFill1SquareFill} color="green.500" boxSize={6} />
-              When you first begin judging, you will not have seen enough projects to compare so you
-              will either
+              <ListIcon as={BsFill1CircleFill} color="green.500" boxSize={6} />
+              Locate the project you need to judge and listen to their amazing pitch.
             </ListItem>
             <ListItem fontSize={20}>
-              <ListIcon as={BsFill2SquareFill} color="green.500" boxSize={6} />
-              When you do get 2 projects to compare, you will select which of the two is better
-              based on the given criteria&quot;
+              <ListIcon as={BsFill2CircleFill} color="green.500" boxSize={6} />
+              After you heard the pitch and judged the project, quickly find you next one.
             </ListItem>
             <ListItem fontSize={20}>
-              <ListIcon as={BsFill3SquareFill} color="green.500" boxSize={6} />
-              Once you made your first vote, find your new &quot;current&quot; team and keep voting
-              until the voting session ends or you seen all the teams
+              <ListIcon as={BsFill3CircleFill} color="green.500" boxSize={6} />
+              Continue judging projects until there&apos;s no more teams to judge or until judging
+              has ended.
             </ListItem>
           </List>
         </Box>
