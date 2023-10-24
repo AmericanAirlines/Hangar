de4ba39940d11ff15e7688dc1f7ff79c3cd78b4a
Ansaar-Syed <137100345+Ansaar-Syed@users.noreply.github.com>
2023-10-24 16:28:27 -0500

    feat: Expo Judge Access Middleware (#572)
    * middleware version 1
    
    * typo correction
    
    * created the middleware
    
    * fixed all tests files
    
    * added middleware test file
    
    * minor changes
    
    * minor changes v2
    
    * Update packages/api/src/middleware/expoJudgeAccessMiddleware.ts
    
    ---------
    
    Co-authored-by: Spencer Kaiser <SpencerKaiser@users.noreply.github.com>

--- packages/api/src/api/expoJudgingSession/id/index.ts
+++ packages/api/src/api/expoJudgingSession/id/index.ts
@@ -6,13 +6,14 @@ import { continueSession } from './continueSession';
 import { results } from './results';
 import { adminMiddleware } from '../../../middleware/adminMiddleware';
 import { judgeMiddleware } from '../../../middleware/judgeMiddleware';
+import { expoJudgeAccessMiddleware } from '../../../middleware/expoJudgeAccessMiddleware';
 
 export const id = Router({ mergeParams: true });
 
 id.use('/results', adminMiddleware, results);
 
 // Judge routes
-id.use(judgeMiddleware);
+id.use(judgeMiddleware, expoJudgeAccessMiddleware);
 id.get('', get);
 id.use('/projects', projects);
 id.use('/skip', skip);
