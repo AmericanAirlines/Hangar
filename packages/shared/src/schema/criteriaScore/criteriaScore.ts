f1414258e8225a86eecd2aa988b0d8ac6214b066
Spencer Kaiser <spencer.kaiser@aa.com>
2023-10-24 12:35:29 -0500

    Removing positive requirement to allow 0 as a score

--- packages/shared/src/schema/criteriaScore/criteriaScore.ts
+++ packages/shared/src/schema/criteriaScore/criteriaScore.ts
@@ -2,5 +2,5 @@ import { z } from 'zod';
 
 export const criteriaScore = z.object({
   criteria: z.string(),
-  score: z.number().int().positive(),
+  score: z.number().int(),
 });
--- packages/shared/tests/schema/criteriaScore/criteriaScore.test.ts
+++ packages/shared/tests/schema/criteriaScore/criteriaScore.test.ts
@@ -4,7 +4,7 @@ describe('criteriaScore schema', () => {
   it('validates a valid object', () => {
     const result = criteriaScore.safeParse({
       criteria: '123',
-      score: 2,
+      score: 0,
     });
     expect(result.success).toEqual(true);
   });
