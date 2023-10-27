244869f55ecc370d158ef79b006d5e6b4b02cd80
Ansaar Syed <Ansaar.Syed@aa.com>
2023-10-27 13:43:59 -0500

    more changes

---
+++ packages/web/src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,20 @@
+import { Heading, Text, Flex } from '@chakra-ui/react';
+import { Project } from '@hangar/shared';
+
+type ProjectCardProps = {
+  project: Project;
+};
+
+export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
+  const { name, description, location, repoUrl } = project;
+
+  return (
+    <Flex>
+      <Heading size="md">{name}</Heading>
+
+      <Text>{description}</Text>
+      <Text>{location}</Text>
+      <Text>{repoUrl}</Text>
+    </Flex>
+  );
+};
