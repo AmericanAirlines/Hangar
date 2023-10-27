a9383f540994fa58ef24c813bcdc50fd5eb839b8
Ansaar Syed <Ansaar.Syed@aa.com>
2023-10-27 14:32:13 -0500

    added more cool stuff

--- packages/web/src/components/ProjectCard/ProjectCard.tsx
+++ packages/web/src/components/ProjectCard/ProjectCard.tsx
@@ -1,4 +1,6 @@
-import { Heading, Text, Flex } from '@chakra-ui/react';
+import { Heading, Text, Flex, Box, Link } from '@chakra-ui/react';
+import { FiExternalLink } from 'react-icons/fi';
+import { motion } from 'framer-motion';
 import { Project } from '@hangar/shared';
 
 type ProjectCardProps = {
@@ -9,12 +11,52 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   const { name, description, location, repoUrl } = project;
 
   return (
-    <Flex direction={'column'}>
-      <Heading size="md">{name}</Heading>
+    <motion.div
+      whileHover={{ y: -10, boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.2)' }}
+      transition={{ duration: 0.3, ease: 'easeInOut' }}
+    >
+      <Box
+        borderRadius="lg"
+        overflow="hidden"
+        p={6}
+        background="linear-gradient(45deg, #4CAF50, #2196F3)"
+        color="white"
+        border="2px solid"
+        borderColor="teal.500"
+        boxShadow="lg"
+        transition="all 0.3s"
+        _hover={{
+          transform: 'scale(1.05)',
+          borderColor: 'teal.300',
+        }}
+      >
+        <Heading as="h2" size="lg" mb={2} textShadow="2px 2px 4px rgba(0, 0, 0, 0.5)">
+          {name}
+        </Heading>
 
-      <Text>{description}</Text>
-      <Text>{location}</Text>
-      <Text>{repoUrl}</Text>
-    </Flex>
+        <Text fontSize="md" mb={4} textShadow="1px 1px 2px rgba(0, 0, 0, 0.5)">
+          {description}
+        </Text>
+
+        <Flex align="center" mb={2}>
+          <Text fontSize="sm" fontWeight="bold" mr={2}>
+            Location:
+          </Text>
+          <Text fontSize="sm" fontWeight="bold">
+            {location}
+          </Text>
+        </Flex>
+
+        <Flex align="center">
+          <Text fontSize="sm" fontWeight="bold" mr={2}>
+            Repo URL:
+          </Text>
+          <Link href={repoUrl} isExternal display="flex" alignItems="center">
+            {repoUrl}
+            <FiExternalLink style={{ marginLeft: '4px' }} />
+          </Link>
+        </Flex>
+      </Box>
+    </motion.div>
   );
 };
