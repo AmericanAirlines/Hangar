e9a3f660137fc4520915300beed1ff99237790a2
Ansaar-Syed <137100345+Ansaar-Syed@users.noreply.github.com>
2023-10-31 11:28:52 -0500

    feat: updated PrizeCard to include winner (#595)

--- packages/web/src/components/Prizes/PrizeCard.tsx
+++ packages/web/src/components/Prizes/PrizeCard.tsx
@@ -1,5 +1,6 @@
-import { ListItem, Heading, Flex, Text } from '@chakra-ui/react';
+import { ListItem, Heading, Flex, Text, Button } from '@chakra-ui/react';
 import { Prize } from '@hangar/shared';
+import { useRouter } from 'next/router';
 import { colors } from '../../theme';
 
 type PrizeCardProps = {
@@ -10,18 +11,30 @@ const RANKING = ['🥇', '🥈', '🥉'];
 const BONUS = '✨';
 
 export const PrizeCard: React.FC<PrizeCardProps> = ({ prize }) => {
-  const { name, description, position, isBonus } = prize;
+  const router = useRouter();
+  const { name, description, position, isBonus, winner } = prize;
 
   return (
     <ListItem background={colors.brandPrimaryDark}>
-      <Heading as="h2" size="md">
-        <Flex gap={2}>
-          <Text>{isBonus ? BONUS : RANKING[position]}</Text>
-          <Text>{name}</Text>
-        </Flex>
-      </Heading>
+      <Flex direction="column" gap={3}>
+        <Heading as="h2" size="md">
+          <Flex gap={2}>
+            <Text>{isBonus ? BONUS : RANKING[position]}</Text>
+            <Text>{name}</Text>
+          </Flex>
+        </Heading>
 
-      <Text>{description}</Text>
+        <Text>{description}</Text>
+        {winner && (
+          <Button
+            onClick={() => {
+              void router.push(`/project/${winner}`);
+            }}
+          >
+            View Winner
+          </Button>
+        )}
+      </Flex>
     </ListItem>
   );
 };
