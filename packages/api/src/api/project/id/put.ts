21235aa3e6be09df9dbb286b54aee68786125e74
Ansaar Syed <Ansaar.Syed@aa.com>
2023-11-09 16:53:00 -0600

    commenting out the API, to be worked in a different PR

--- packages/api/src/api/project/id/put.ts
+++ packages/api/src/api/project/id/put.ts
@@ -1,11 +1,10 @@
-import { Request, Response } from "express";
-import { DriverException, LockMode } from "@mikro-orm/core";
-import { Project } from "@hangar/database";
-import { Schema } from "@hangar/shared";
-import { logger } from "../../../utils/logger";
-import { validatePayload } from "../../../utils/validatePayload";
+// import { Request, Response } from 'express';
+// import { DriverException, LockMode } from '@mikro-orm/core';
+// import { Project } from '@hangar/database';
+// import { Schema } from '@hangar/shared';
+// import { logger } from '../../../utils/logger';
+// import { validatePayload } from '../../../utils/validatePayload';
 
-export const put = async (req: Request, res: Response) => {
-    const {entityManager, params: {id}} = req;
-    return void
-}
+// const put = () => {
+//   return undefined;
+// };
