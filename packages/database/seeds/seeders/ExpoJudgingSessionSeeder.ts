925870deba8ab6f52eb37819db06ac7cccd1f1f4
Spencer Kaiser <spencer.kaiser@aa.com>
2023-12-07 11:37:31 -0600

    fixing lint errorss

--- packages/database/seeds/seeders/ExpoJudgingSessionSeeder.ts
+++ packages/database/seeds/seeders/ExpoJudgingSessionSeeder.ts
@@ -4,11 +4,13 @@ import { Seeder } from '@mikro-orm/seeder';
 import { env } from '../env';
 import { ExpoJudgingSession, Project, User } from '../../src';
 
+const numSessions = 2;
+
 export class ExpoJudgingSessionSeeder extends Seeder {
   run = async (em: EntityManager): Promise<void> => {
     if (env.primaryUserIsAdmin) {
       try {
-        for(let i=0;i<2;i=i+1){
+        for (let i = 0; i < numSessions; i += 1) {
           const admin = await em.findOneOrFail(User, { id: '1' });
           const expoJudgingSession = new ExpoJudgingSession({
             createdBy: ref(admin),
