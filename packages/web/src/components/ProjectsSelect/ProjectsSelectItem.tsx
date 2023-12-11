ab7f6fb8a020054630401b40fde32cf482f537c3
Spencer Kaiser <spencer.kaiser@aa.com>
2023-12-11 10:47:57 -0600

    feat: adding component for selecting projects

---
+++ packages/web/src/components/ProjectsSelect/ProjectsSelectItem.tsx
@@ -0,0 +1,45 @@
+import { Checkbox, Flex, Text } from '@chakra-ui/react';
+import { Config, Project } from '@hangar/shared';
+import React from 'react';
+import { ProjectsSelectStyleContext } from './ProjectsSelectStyleContext';
+
+type ProjectsSelectItemProps = {
+  project: Project;
+  isSelected: boolean;
+  onClick: () => void;
+};
+
+export const ProjectsSelectItem: React.FC<ProjectsSelectItemProps> = ({
+  project,
+  isSelected,
+  onClick,
+}) => {
+  const { rowPadding, checkboxSize, checkboxPadding } = React.useContext(
+    ProjectsSelectStyleContext,
+  );
+
+  return (
+    <Flex px={rowPadding}>
+      <Checkbox
+        size={checkboxSize}
+        isChecked={isSelected}
+        onChange={onClick}
+        px={checkboxPadding}
+      />
+      <Flex
+        direction={{ base: 'column', md: 'row' }}
+        alignItems={{ base: 'baseline', md: 'center' }}
+        gap={{ base: 1, md: 3 }}
+        onClick={onClick}
+        w="full"
+      >
+        <Text>{project.name}</Text>
+        {!!project.location && (
+          <Text fontSize="xs">
+            ({Config.project.locationLabel}: {project.location})
+          </Text>
+        )}
+      </Flex>
+    </Flex>
+  );
+};
