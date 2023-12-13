48fab41bab237d71842adf81148a7995249ab512
Spencer Kaiser <spencer.kaiser@aa.com>
2023-12-13 16:40:03 -0600

    fix: fixing schema to require at least one project

--- packages/shared/tests/schema/expoJudgingSession/post.test.ts
+++ packages/shared/tests/schema/expoJudgingSession/post.test.ts
@@ -4,4 +4,9 @@ describe('expoJudgingSession post schema', () => {
   it('validates expoJudgingSession with array of projectIds correctly', () => {
     expect(Schema.expoJudgingSession.post.safeParse({ projectIds: ['1', '2'] }).success).toBe(true);
   });
+
+  it('invalidates an empty array and a missing projectIds array', () => {
+    expect(Schema.expoJudgingSession.post.safeParse({ projectIds: [] }).success).toBe(false);
+    expect(Schema.expoJudgingSession.post.safeParse({}).success).toBe(false);
+  });
 });
