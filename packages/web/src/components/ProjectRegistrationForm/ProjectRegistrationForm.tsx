3575a2cb976508e7aa35240ea9bb29214ff9df8b
Ansaar Syed <Ansaar.Syed@aa.com>
2023-12-27 16:21:20 -0600

    made changes to modal

--- packages/web/src/components/ProjectRegistrationForm/ProjectRegistrationForm.tsx
+++ packages/web/src/components/ProjectRegistrationForm/ProjectRegistrationForm.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable max-lines */
 /* eslint-disable no-underscore-dangle */
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Input,
   FormControl,
@@ -9,6 +10,7 @@ import {
   Textarea,
   InputProps,
   TextareaProps,
+  useClipboard,
 } from '@chakra-ui/react';
 import { Config } from '@hangar/shared';
 import { RegistrationFormProps, useProjectRegistrationForm } from './useProjectRegistrationForm';
@@ -41,6 +43,12 @@ export const ProjectRegistrationForm: React.FC<RegistrationFormProps> = ({
     project,
     onComplete,
   });
+
+  // State to hold the contributors invite code
+  const [contributorsInviteCode, setContributorsInviteCode] = useState<string>('');
+
+  // Chakra UI hook to copy the invite code to the clipboard
+  const { onCopy } = useClipboard(contributorsInviteCode);
   return (
     <form onSubmit={formik.handleSubmit}>
       <VStack alignItems="stretch">
@@ -80,6 +88,24 @@ export const ProjectRegistrationForm: React.FC<RegistrationFormProps> = ({
           </FormLabel>
           <Input {...(formElementProps({ key: 'repoUrl', formik, errors }) as InputProps)} />
           <FormHelperHint error={errors?.repoUrl?._errors} />
+        </FormControl>
+
+        <FormControl id="contributorsInviteCode">
+          <FormLabel>
+            Contributors Invite Code
+            <HintTooltip>
+              This code is used to invite contributors to your project. You can copy it and send it
+              to your contributors.
+            </HintTooltip>
+          </FormLabel>
+          <Input
+            value={contributorsInviteCode}
+            onChange={(e) => setContributorsInviteCode(e.target.value)}
+            variant="filled"
+            type="text"
+            isReadOnly
+          />
+          <Button onClick={onCopy}>Copy Invite Code</Button>
         </FormControl>
 
         <Button type="submit" isLoading={isLoading}>
